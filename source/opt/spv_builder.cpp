d7a6b5f2f30a13a4b0b61df11c4ff45bd9199f3a
Lei Zhang <antiagainst@google.com>
2016-05-22 14:17:39 -0400

    Add a builder class for constructing the in-memory representation.

---
+++ source/opt/spv_builder.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2016 Google Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and/or associated documentation files (the
+// "Materials"), to deal in the Materials without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Materials, and to
+// permit persons to whom the Materials are furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Materials.
+//
+// MODIFICATIONS TO THIS FILE MAY MEAN IT NO LONGER ACCURATELY REFLECTS
+// KHRONOS STANDARDS. THE UNMODIFIED, NORMATIVE VERSIONS OF KHRONOS
+// SPECIFICATIONS AND HEADER INFORMATION ARE LOCATED AT
+//    https://www.khronos.org/registry/
+//
+// THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+
+#include <cassert>
+
+#include "spv_builder.h"
+
+namespace spvtools {
+namespace opt {
+namespace ir {
+
+namespace {
+
+inline bool IsDebugInst(SpvOp opcode) {
+  return (opcode >= SpvOpSourceContinued && opcode <= SpvOpLine) ||
+         opcode == SpvOpNoLine || opcode == SpvOpModuleProcessed;
+}
+inline bool IsAnnotationInst(SpvOp opcode) {
+  return opcode >= SpvOpDecorate && opcode <= SpvOpGroupMemberDecorate;
+}
+inline bool IsTypeInst(SpvOp opcode) {
+  return (opcode >= SpvOpTypeVoid && opcode <= SpvOpTypeForwardPointer) ||
+         opcode == SpvOpTypePipeStorage || opcode == SpvOpTypeNamedBarrier;
+}
+inline bool IsConstantInst(SpvOp opcode) {
+  return opcode >= SpvOpConstantTrue && opcode <= SpvOpSpecConstantOp;
+}
+inline bool IsTerminatorInst(SpvOp opcode) {
+  return opcode >= SpvOpBranch && opcode <= SpvOpUnreachable;
+}
+
+}  // anonymous namespace
+
+void SpvBuilder::AddInstruction(const spv_parsed_instruction_t* inst) {
+  const auto opcode = static_cast<SpvOp>(inst->opcode);
+  // Handle function and basic block boundaries first, then normal
+  // instructions.
+  if (opcode == SpvOpFunction) {
+    assert(function_ == nullptr);
+    assert(block_ == nullptr);
+    function_.reset(new Function(module_, Inst(*inst)));
+  } else if (opcode == SpvOpFunctionEnd) {
+    assert(function_ != nullptr);
+    assert(block_ == nullptr);
+    module_->AddFunction(std::move(*function_.release()));
+    function_ = nullptr;
+  } else if (opcode == SpvOpLabel) {
+    assert(function_ != nullptr);
+    assert(block_ == nullptr);
+    block_.reset(new BasicBlock(function_.get(), Inst(*inst)));
+  } else if (IsTerminatorInst(opcode)) {
+    assert(function_ != nullptr);
+    assert(block_ != nullptr);
+    block_->AddInstruction(Inst(*inst));
+    function_->AddBasicBlock(std::move(*block_.release()));
+    block_ = nullptr;
+  } else {
+    if (function_ == nullptr) {  // Outside function definition
+      assert(block_ == nullptr);
+      if (opcode == SpvOpCapability) {
+        module_->AddCapability(Inst(*inst));
+      } else if (opcode == SpvOpExtension) {
+        module_->AddExtension(Inst(*inst));
+      } else if (opcode == SpvOpExtInstImport) {
+        module_->AddExtInstSet(Inst(*inst));
+      } else if (opcode == SpvOpMemoryModel) {
+        module_->SetMemoryModel(Inst(*inst));
+      } else if (opcode == SpvOpEntryPoint) {
+        module_->AddEntryPoint(Inst(*inst));
+      } else if (opcode == SpvOpExecutionMode) {
+        module_->AddExecutionMode(Inst(*inst));
+      } else if (IsDebugInst(opcode)) {
+        module_->AddDebugInst(Inst(*inst));
+      } else if (IsAnnotationInst(opcode)) {
+        module_->AddAnnotationInst(Inst(*inst));
+      } else if (IsTypeInst(opcode)) {
+        module_->AddType(Inst(*inst));
+      } else if (IsConstantInst(opcode)) {
+        module_->AddConstant(Inst(*inst));
+      } else if (opcode == SpvOpVariable) {
+        module_->AddVariable(Inst(*inst));
+      } else {
+        assert(0 && "unhandled inst type outside function defintion");
+      }
+    } else {
+      if (block_ == nullptr) {  // Inside function but outside blocks
+        assert(opcode == SpvOpFunctionParameter);
+        function_->AddParameter(Inst(*inst));
+      } else {
+        block_->AddInstruction(Inst(*inst));
+      }
+    }
+  }
+}
+
+}  // namespace ir
+}  // namespace opt
+}  // namespace spvtools
---
+++ source/opt/spv_builder.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2016 Google Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and/or associated documentation files (the
+// "Materials"), to deal in the Materials without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Materials, and to
+// permit persons to whom the Materials are furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Materials.
+//
+// MODIFICATIONS TO THIS FILE MAY MEAN IT NO LONGER ACCURATELY REFLECTS
+// KHRONOS STANDARDS. THE UNMODIFIED, NORMATIVE VERSIONS OF KHRONOS
+// SPECIFICATIONS AND HEADER INFORMATION ARE LOCATED AT
+//    https://www.khronos.org/registry/
+//
+// THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+
+#ifndef LIBSPIRV_OPT_SPVBUILDER_H_
+#define LIBSPIRV_OPT_SPVBUILDER_H_
+
+#include <memory>
+
+#include "constructs.h"
+#include "spirv-tools/libspirv.h"
+
+namespace spvtools {
+namespace opt {
+namespace ir {
+
+class SpvBuilder {
+ public:
+  SpvBuilder(Module* module) : module_(module) {}
+
+  void SetModuleHeader(uint32_t magic, uint32_t version, uint32_t generator,
+                       uint32_t bound, uint32_t reserved) {
+    module_->SetHeader({magic, version, generator, bound, reserved});
+  }
+  void AddInstruction(const spv_parsed_instruction_t* inst);
+
+ private:
+  Module* module_;
+  std::unique_ptr<Function> function_;
+  std::unique_ptr<BasicBlock> block_;
+};
+
+}  // namespace ir
+}  // namespace opt
+}  // namespace spvtools
+
+#endif  // LIBSPIRV_OPT_SPVBUILDER_H_
