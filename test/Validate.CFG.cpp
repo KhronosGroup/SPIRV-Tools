ecf4a0e570259722d7a538b8680ca1c4a4d3bd36
Umar Arshad <umar@arrayfire.com>
2016-06-01 18:05:28 -0400

    Additional CFG tests
    * Empty function
    * Single block loops
    * Nested loops
    * Nested selection

--- test/Validate.CFG.cpp
+++ test/Validate.CFG.cpp
@@ -87,7 +87,7 @@ class Block {
   ///
   /// @param[in]: label the label id of the block
   /// @param[in]: type the branch instruciton that ends the block
-  Block(string label, SpvOp type = SpvOpBranch)
+  explicit Block(string label, SpvOp type = SpvOpBranch)
       : label_(label), body_(), type_(type), successors_() {}
 
   /// Sets the instructions which will appear in the body of the block
@@ -120,16 +120,19 @@ class Block {
         out << ss.str();
       } break;
       case SpvOpReturn:
+        assert(successors_.size() == 0);
         out << "OpReturn\n";
         break;
       case SpvOpUnreachable:
+        assert(successors_.size() == 0);
         out << "OpUnreachable\n";
         break;
       case SpvOpBranch:
+        assert(successors_.size() == 1);
         out << "OpBranch %" + successors_.front().label_;
         break;
       default:
-        assert(1 != 1 && "Unhandled");
+        assert(1 != 0 && "Unhandled");
     }
     out << "\n";
 
@@ -581,10 +584,100 @@ TEST_F(ValidateCFG, UnreachableBranch) {
   CompileSuccessfully(str);
   ASSERT_EQ(SPV_SUCCESS, ValidateInstructions());
 }
-/// TODO(umar): Empty function
-/// TODO(umar): Single block loops
-/// TODO(umar): Nested loops
-/// TODO(umar): Nested selection
+
+TEST_F(ValidateCFG, EmptyFunction) {
+  string str = header + types_consts +
+               "%func    = OpFunction %voidt None %funct\n" + "OpFunctionEnd\n";
+
+  CompileSuccessfully(str);
+  ASSERT_EQ(SPV_SUCCESS, ValidateInstructions());
+}
+
+TEST_F(ValidateCFG, SingleBlockLoop) {
+  Block entry("entry");
+  Block loop("loop", SpvOpBranchConditional);
+  Block exit("exit", SpvOpReturn);
+
+  loop.setBody(
+      "%cond    = OpSLessThan %intt %one %two\n"
+      "OpLoopMerge %exit %loop None\n");
+
+  string str =
+      header + types_consts + "%func    = OpFunction %voidt None %funct\n";
+
+  str += entry >> loop;
+  str += loop >> vector<Block>({loop, exit});
+  str += exit;
+  str += "OpFunctionEnd";
+
+  CompileSuccessfully(str);
+  ASSERT_EQ(SPV_SUCCESS, ValidateInstructions());
+}
+
+TEST_F(ValidateCFG, NestedLoops) {
+  Block entry("entry");
+  Block loop1("loop1", SpvOpBranchConditional);
+  Block loop2("loop2", SpvOpBranchConditional);
+  Block loop2_merge("loop2_merge");
+  Block exit("exit", SpvOpReturn);
+
+  loop1.setBody(
+      "%cond    = OpSLessThan %intt %one %two\n"
+      "OpLoopMerge %exit %loop2 None\n");
+
+  loop2.setBody("OpLoopMerge %loop2_merge %loop2 None\n");
+
+  string str =
+      header + types_consts + "%func    = OpFunction %voidt None %funct\n";
+
+  str += entry >> loop1;
+  str += loop1 >> vector<Block>({loop2, exit});
+  str += loop2 >> vector<Block>({loop2, loop2_merge});
+  str += loop2_merge >> exit;
+  str += exit;
+  str += "OpFunctionEnd";
+
+  CompileSuccessfully(str);
+  ASSERT_EQ(SPV_SUCCESS, ValidateInstructions());
+}
+
+TEST_F(ValidateCFG, NestedSelection) {
+  Block entry("entry");
+  const int N = 256;
+  vector<Block> if_blocks;
+  vector<Block> merge_blocks;
+  Block inner("inner");
+
+  if_blocks.emplace_back("if0", SpvOpBranchConditional);
+  if_blocks[0].setBody(
+      "%cond    = OpSLessThan %intt %one %two\n"
+      "OpSelectionMerge %if_merge0 None\n");
+  merge_blocks.emplace_back("if_merge0", SpvOpReturn);
+
+  for (int i = 1; i < N; i++) {
+    stringstream ss;
+    ss << i;
+    if_blocks.emplace_back("if" + ss.str(), SpvOpBranchConditional);
+    if_blocks[i].setBody("OpSelectionMerge %if_merge" + ss.str() + " None\n");
+    merge_blocks.emplace_back("if_merge" + ss.str(), SpvOpBranch);
+  }
+  string str =
+      header + types_consts + "%func    = OpFunction %voidt None %funct\n";
+
+  for (int i = 0; i < N - 1; i++) {
+    str += if_blocks[i] >> vector<Block>({if_blocks[i + 1], merge_blocks[i]});
+  }
+  str += if_blocks.back() >> vector<Block>({inner, merge_blocks.back()});
+  str += inner >> merge_blocks.back();
+  for (int i = N - 1; i > 0; i--) {
+    str += merge_blocks[i] >> merge_blocks[i - 1];
+  }
+  str += merge_blocks[0];
+  str += "OpFunctionEnd";
+
+  CompileSuccessfully(str);
+  ASSERT_EQ(SPV_SUCCESS, ValidateInstructions());
+}
 /// TODO(umar): Switch instructions
 /// TODO(umar): CFG branching outside of CFG construct
 /// TODO(umar): Nested CFG constructs
