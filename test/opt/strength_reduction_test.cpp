6df391aabb11f3e2f3ee9ee6d47905d0ed97f42b
Steven Perron <stevenperron@google.com>
2017-09-14 11:37:08 -0400

    Fix up the unit test.
    Changed the unit test to use the gmock framework more extensively.

--- test/opt/strength_reduction_test.cpp
+++ test/opt/strength_reduction_test.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "assembly_builder.h"
+#include "gmock/gmock.h"
 #include "pass_fixture.h"
 #include "pass_utils.h"
 
@@ -26,29 +27,31 @@ namespace {
 
 using namespace spvtools;
 
+using ::testing::HasSubstr;
+using ::testing::MatchesRegex;
+
 using StrengthReductionBasicTest = PassTest<::testing::Test>;
 
 // Test to make sure we replace 5*8.
 TEST_F(StrengthReductionBasicTest, BasicReplaceMulBy8a) {
   const std::vector<const char*> text = {
       // clang-format off
-             "OpCapability Shader",
-             "OpCapability Float64",
-        "%1 = OpExtInstImport \"GLSL.std.450\"",
-             "OpMemoryModel Logical GLSL450",
-             "OpEntryPoint Vertex %2 \"main\"",
-             "OpName %2 \"main\"",
-        "%3 = OpTypeVoid",
-        "%4 = OpTypeFunction %3",
-        "%5 = OpTypeInt 32 1",
-        "%6 = OpTypeInt 32 0",
-        "%7 = OpConstant %5 5",
-        "%8 = OpConstant %5 8",
-        "%2 = OpFunction %3 None %4",
-        "%9 = OpLabel",
-        "%10 = OpIMul %5 %7 %8",
-             "OpReturn",
-             "OpFunctionEnd",
+               "OpCapability Shader",
+               "OpCapability Float64",
+          "%1 = OpExtInstImport \"GLSL.std.450\"",
+               "OpMemoryModel Logical GLSL450",
+               "OpEntryPoint Vertex %main \"main\"",
+               "OpName %main \"main\"",
+       "%void = OpTypeVoid",
+          "%4 = OpTypeFunction %void",
+       "%uint = OpTypeInt 32 0",
+     "%uint_5 = OpConstant %uint 5",
+     "%uint_8 = OpConstant %uint 8",
+       "%main = OpFunction %void None %4",
+          "%8 = OpLabel",
+          "%9 = OpIMul %uint %uint_5 %uint_8",
+               "OpReturn",
+               "OpFunctionEnd"
       // clang-format on
   };
 
@@ -57,33 +60,30 @@ TEST_F(StrengthReductionBasicTest, BasicReplaceMulBy8a) {
 
   EXPECT_EQ(opt::Pass::Status::SuccessWithChange, std::get<1>(result));
   const std::string& output = std::get<0>(result);
-  std::string::size_type pos = output.find("OpIMul");
-  EXPECT_EQ(pos, std::string::npos);
-  pos = output.find("OpShiftLeftLogical");
-  EXPECT_NE(pos, std::string::npos);
+  EXPECT_THAT(output, Not(HasSubstr("OpIMul")));
+  EXPECT_THAT(output, HasSubstr("OpShiftLeftLogical %uint %uint_5 %uint_3"));
 }
 
 // Test to make sure we replace 8*5.
 TEST_F(StrengthReductionBasicTest, BasicReplaceMulBy8b) {
   const std::vector<const char*> text = {
       // clang-format off
-             "OpCapability Shader",
-             "OpCapability Float64",
-        "%1 = OpExtInstImport \"GLSL.std.450\"",
-             "OpMemoryModel Logical GLSL450",
-             "OpEntryPoint Vertex %2 \"main\"",
-             "OpName %2 \"main\"",
-        "%3 = OpTypeVoid",
-        "%4 = OpTypeFunction %3",
-        "%5 = OpTypeInt 32 1",
-        "%6 = OpTypeInt 32 0",
-        "%7 = OpConstant %5 8",
-        "%8 = OpConstant %5 5",
-        "%2 = OpFunction %3 None %4",
-        "%9 = OpLabel",
-        "%10 = OpIMul %5 %7 %8",
-             "OpReturn",
-             "OpFunctionEnd",
+               "OpCapability Shader",
+               "OpCapability Float64",
+          "%1 = OpExtInstImport \"GLSL.std.450\"",
+               "OpMemoryModel Logical GLSL450",
+               "OpEntryPoint Vertex %main \"main\"",
+               "OpName %main \"main\"",
+       "%void = OpTypeVoid",
+          "%4 = OpTypeFunction %void",
+        "%int = OpTypeInt 32 1",
+      "%int_16 = OpConstant %int 16",
+      "%int_5 = OpConstant %int 5",
+       "%main = OpFunction %void None %4",
+          "%8 = OpLabel",
+          "%9 = OpIMul %int %int_16 %int_5",
+               "OpReturn",
+               "OpFunctionEnd"
       // clang-format on
   };
 
@@ -92,10 +92,37 @@ TEST_F(StrengthReductionBasicTest, BasicReplaceMulBy8b) {
 
   EXPECT_EQ(opt::Pass::Status::SuccessWithChange, std::get<1>(result));
   const std::string& output = std::get<0>(result);
-  std::string::size_type pos = output.find("OpIMul");
-  EXPECT_EQ(pos, std::string::npos);
-  pos = output.find("OpShiftLeftLogical");
-  EXPECT_NE(pos, std::string::npos);
+  EXPECT_THAT(output, Not(HasSubstr("OpIMul")));
+  EXPECT_THAT(output, HasSubstr("OpShiftLeftLogical %int %int_5 %uint_4"));
+}
+
+// Test to make sure we don't replace 0*5.
+TEST_F(StrengthReductionBasicTest, BasicDontReplace0) {
+  const std::vector<const char*> text = {
+      // clang-format off
+               "OpCapability Shader",
+               "OpCapability Float64",
+          "%1 = OpExtInstImport \"GLSL.std.450\"",
+               "OpMemoryModel Logical GLSL450",
+               "OpEntryPoint Vertex %main \"main\"",
+               "OpName %main \"main\"",
+       "%void = OpTypeVoid",
+          "%4 = OpTypeFunction %void",
+        "%int = OpTypeInt 32 1",
+      "%int_0 = OpConstant %int 0",
+      "%int_5 = OpConstant %int 5",
+       "%main = OpFunction %void None %4",
+          "%8 = OpLabel",
+          "%9 = OpIMul %int %int_0 %int_5",
+               "OpReturn",
+               "OpFunctionEnd"
+      // clang-format on
+  };
+
+  auto result = SinglePassRunAndDisassemble<opt::StrengthReductionPass>(
+      JoinAllInsts(text), /* skip_nop = */ true);
+
+  EXPECT_EQ(opt::Pass::Status::SuccessWithoutChange, std::get<1>(result));
 }
 
 // Test to make sure we do not replace a multiple of 5 and 7.
@@ -128,26 +155,26 @@ TEST_F(StrengthReductionBasicTest, BasicNoChange) {
   EXPECT_EQ(opt::Pass::Status::SuccessWithoutChange, std::get<1>(result));
 }
 
-// Test to make sure constants are reused and not duplicated.
-TEST_F(StrengthReductionBasicTest, NoDuplicateConstants) {
+// Test to make sure constants and types are reused and not duplicated.
+TEST_F(StrengthReductionBasicTest, NoDuplicateConstantsAndTypes) {
   const std::vector<const char*> text = {
       // clang-format off
-             "OpCapability Shader",
-             "OpCapability Float64",
-        "%1 = OpExtInstImport \"GLSL.std.450\"",
-             "OpMemoryModel Logical GLSL450",
-             "OpEntryPoint Vertex %2 \"main\"",
-             "OpName %2 \"main\"",
-        "%3 = OpTypeVoid",
-        "%4 = OpTypeFunction %3",
-        "%6 = OpTypeInt 32 0",
-        "%7 = OpConstant %6 8",
-        "%8 = OpConstant %6 3",
-        "%2 = OpFunction %3 None %4",
-        "%9 = OpLabel",
-        "%10 = OpIMul %6 %7 %8",
-             "OpReturn",
-             "OpFunctionEnd",
+               "OpCapability Shader",
+               "OpCapability Float64",
+          "%1 = OpExtInstImport \"GLSL.std.450\"",
+               "OpMemoryModel Logical GLSL450",
+               "OpEntryPoint Vertex %main \"main\"",
+               "OpName %main \"main\"",
+       "%void = OpTypeVoid",
+          "%4 = OpTypeFunction %void",
+       "%uint = OpTypeInt 32 0",
+     "%uint_8 = OpConstant %uint 8",
+     "%uint_3 = OpConstant %uint 3",
+       "%main = OpFunction %void None %4",
+          "%8 = OpLabel",
+          "%9 = OpIMul %uint %uint_8 %uint_3",
+               "OpReturn",
+               "OpFunctionEnd",
       // clang-format on
   };
 
@@ -156,43 +183,8 @@ TEST_F(StrengthReductionBasicTest, NoDuplicateConstants) {
 
   EXPECT_EQ(opt::Pass::Status::SuccessWithChange, std::get<1>(result));
   const std::string& output = std::get<0>(result);
-  std::string::size_type pos1 = output.find("OpConstant %uint 3");
-  EXPECT_NE(pos1, std::string::npos);
-  std::string::size_type pos2 = output.rfind("OpConstant %uint 3");
-  EXPECT_EQ(pos1, pos2);
+  EXPECT_THAT(output, Not(MatchesRegex(".*OpConstant %uint 3.*OpConstant %uint 3.*")));
+  EXPECT_THAT(output, Not(MatchesRegex(".*OpTypeInt 32 0.*OpTypeInt 32 0.*")));
 }
 
-// Test to make sure types are reused and not duplicated.
-TEST_F(StrengthReductionBasicTest, NoDuplicateTypes) {
-  const std::vector<const char*> text = {
-      // clang-format off
-             "OpCapability Shader",
-             "OpCapability Float64",
-        "%1 = OpExtInstImport \"GLSL.std.450\"",
-             "OpMemoryModel Logical GLSL450",
-             "OpEntryPoint Vertex %2 \"main\"",
-             "OpName %2 \"main\"",
-        "%3 = OpTypeVoid",
-        "%4 = OpTypeFunction %3",
-        "%6 = OpTypeInt 32 0",
-        "%7 = OpConstant %6 8",
-        "%8 = OpConstant %6 3",
-        "%2 = OpFunction %3 None %4",
-        "%9 = OpLabel",
-        "%10 = OpIMul %6 %7 %8",
-             "OpReturn",
-             "OpFunctionEnd",
-      // clang-format on
-  };
-
-  auto result = SinglePassRunAndDisassemble<opt::StrengthReductionPass>(
-      JoinAllInsts(text), /* skip_nop = */ true);
-
-  EXPECT_EQ(opt::Pass::Status::SuccessWithChange, std::get<1>(result));
-  const std::string& output = std::get<0>(result);
-  std::string::size_type pos1 = output.find("OpTypeInt 32 0");
-  EXPECT_NE(pos1, std::string::npos);
-  std::string::size_type pos2 = output.rfind("OpTypeInt 32 0");
-  EXPECT_EQ(pos1, pos2);
-}
 }  // anonymous namespace
