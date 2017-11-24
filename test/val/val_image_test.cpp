c170afd93b1d69914f911f225ddaf5bab2640208
Andrey Tuganov <andreyt@google.com>
2017-11-24 10:55:05 -0500

    Relaxed OpImageWrite texel type check

--- test/val/val_image_test.cpp
+++ test/val/val_image_test.cpp
@@ -2524,10 +2524,10 @@ TEST_F(ValidateImage, WriteCoordinateSizeTooSmall) {
       "ImageWrite"));
 }
 
-TEST_F(ValidateImage, WriteTexelNotVector) {
+TEST_F(ValidateImage, WriteTexelWrongType) {
   const std::string body = R"(
 %img = OpLoad %type_image_u32_2d_0000 %uniform_image_u32_2d_0000
-%res1 = OpImageWrite %img %u32vec2_01 %u32_0
+%res1 = OpImageWrite %img %u32vec2_01 %img
 )";
 
   const std::string extra = "\nOpCapability StorageImageWriteWithoutFormat\n";
@@ -2537,7 +2537,7 @@ TEST_F(ValidateImage, WriteTexelNotVector) {
       "Expected Texel to be int or float vector or scalar: ImageWrite"));
 }
 
-TEST_F(ValidateImage, WriteTexelNotVector4) {
+TEST_F(ValidateImage, DISABLED_WriteTexelNotVector4) {
   const std::string body = R"(
 %img = OpLoad %type_image_u32_2d_0000 %uniform_image_u32_2d_0000
 %res1 = OpImageWrite %img %u32vec2_01 %u32vec3_012
