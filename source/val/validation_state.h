3c2e4c7d997404cb8d43299117373a697c28fde1
David Neto <dneto@google.com>
2017-11-24 14:18:17 -0500

    Fix validation of group ops in SPV_AMD_shader_ballot
    This needs custom code since the rules from the extension
    are not encoded in the grammar.
    
    Changes are:
    - The new group instructions don't require Group capability
      when the extension is declared.
    - The Reduce, InclusiveScan, ExclusiveScan normally require the Kernel
      capability, but don't when the extension is declared.
    
    Fixes https://github.com/KhronosGroup/SPIRV-Tools/issues/991

--- source/val/validation_state.h
+++ source/val/validation_state.h
@@ -69,6 +69,9 @@ class ValidationState_t {
     // Allow functionalities enabled by VariablePointersStorageBuffer
     // capability.
     bool variable_pointers_storage_buffer = false;
+
+    // Permit group oerations Reduce, InclusiveScan, ExclusiveScan
+    bool group_ops_reduce_and_scans = false;
   };
 
   ValidationState_t(const spv_const_context context,
