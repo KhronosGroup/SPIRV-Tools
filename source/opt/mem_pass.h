08699920ad473913702948d6191897f21f96f197
Diego Novillo <dnovillo@google.com>
2018-02-12 13:21:48 -0500

    Cleanup.  Use proper #include guard.  NFC.

--- source/opt/mem_pass.h
+++ source/opt/mem_pass.h
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef LIBSPIRV_OPT_OPT_PASS_H_
-#define LIBSPIRV_OPT_OPT_PASS_H_
+#ifndef LIBSPIRV_OPT_MEM_PASS_H_
+#define LIBSPIRV_OPT_MEM_PASS_H_
 
 #include <algorithm>
 #include <list>
@@ -216,4 +216,4 @@ class MemPass : public Pass {
 }  // namespace opt
 }  // namespace spvtools
 
-#endif  // LIBSPIRV_OPT_OPT_PASS_H_
+#endif  // LIBSPIRV_OPT_MEM_PASS_H_
