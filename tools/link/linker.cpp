13391e89c2f9aa3e61d005dde09adc52f74e660a
Pierre Moreau <dev@pmoreau.org>
2018-02-13 22:04:50 +0100

    tools/linker: Allow setting --verify-ids on the command line

--- tools/link/linker.cpp
+++ tools/link/linker.cpp
@@ -36,6 +36,7 @@ Options:
   -h, --help       Print this help.
   -o               Name of the resulting linked SPIR-V binary.
   --create-library Link the binaries into a library, keeping all exported symbols.
+  --verify-ids     Verify that IDs in the resulting modules are truly unique.
   --version        Display linker version information
   --target-env     {vulkan1.0|spv1.0|spv1.1|spv1.2|opencl2.1|opencl2.2}
                    Use Vulkan1.0/SPIR-V1.0/SPIR-V1.1/SPIR-V1.2/OpenCL-2.1/OpenCL2.2 validation rules.
@@ -70,6 +71,8 @@ int main(int argc, char** argv) {
         }
       } else if (0 == strcmp(cur_arg, "--create-library")) {
         options.SetCreateLibrary(true);
+      } else if (0 == strcmp(cur_arg, "--verify-ids")) {
+        options.SetVerifyIds(true);
       } else if (0 == strcmp(cur_arg, "--version")) {
         printf("%s\n", spvSoftwareVersionDetailsString());
         // TODO(dneto): Add OpenCL 2.2 at least.
