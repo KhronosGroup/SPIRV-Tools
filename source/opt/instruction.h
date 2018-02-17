6b376bb7b44866c76ed52733334d54c04e8e6ded
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2018-02-17 11:55:54 -0800

    Add folding for redundant add/sub/mul/div/mix operations
    This change implements instruction folding for arithmetic operations
    that are redundant, specifically:
    
      x + 0 = 0 + x = x
      x - 0 = x
      0 - x = -x
      x * 0 = 0 * x = 0
      x * 1 = 1 * x = x
      0 / x = 0
      x / 1 = x
      mix(a, b, 0) = a
      mix(a, b, 1) = b

--- source/opt/instruction.h
+++ source/opt/instruction.h
@@ -372,6 +372,11 @@ class Instruction : public utils::IntrusiveNodeBase<Instruction> {
   // constant value by |FoldScalar|.
   bool IsFoldableByFoldScalar() const;
 
+  // Returns true if we are allowed to fold or otherwise manipulate the
+  // instruction that defines |id| in the given context. This includes not
+  // handling NaN values.
+  bool IsFloatingPointFoldingAllowed() const;
+
   inline bool operator==(const Instruction&) const;
   inline bool operator!=(const Instruction&) const;
   inline bool operator<(const Instruction&) const;
