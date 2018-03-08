d41a3568c6f181253e1afcbbd42a036d33b91449
David Neto <dneto@google.com>
2018-03-08 15:33:28 -0500

    Add recent Google extensions to optimizer whitelists
    Optimizations should work in the presence of recent
    SPV_GOOGLE_decorate_string and SPV_GOOGLE_hlsl_functionality1
    
    SPV_GOOGLE_decorate_string:
    - Adds operation OpDecorateStringGOOGLE to decorate an object with decorations
      having string operands.
    
    SPV_GOOGLE_hlsl_functionality1:
    - Adds HlslSemanticGOOGLE, used to decorate an interface variable with
      an HLSL semantic string.  Optimizations already preserve those variables
      as required because they are interface variables (with uses), independent
      of whether they have HLSL decorations.
    
    - Adds HlslCounterBufferGOOGLE, used to associate a buffer with a
      counter variable.
    
    Fixes #1391

--- source/opt/local_ssa_elim_pass.cpp
+++ source/opt/local_ssa_elim_pass.cpp
@@ -103,6 +103,8 @@ void LocalMultiStoreElimPass::InitExtensions() {
       "SPV_AMD_shader_fragment_mask",
       "SPV_EXT_fragment_fully_covered",
       "SPV_AMD_gpu_shader_half_float_fetch",
+      "SPV_GOOGLE_decorate_string",
+      "SPV_GOOGLE_hlsl_functionality1",
   });
 }
 
