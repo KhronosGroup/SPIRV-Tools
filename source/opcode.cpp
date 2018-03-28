1e1373e3b5b7737a82212df733aa559e0d8b4a91
Lei Zhang <antiagainst@google.com>
2018-03-28 16:42:44 -0400

    Use standard SPIR-V version scheme for version requirement
    Previously we use symbols in spv_target_env as the minimum version
    requirements for features. That makes version check implicitly
    relies on the order of entries in the spv_target_env enum, which
    also contains client APIs. Instead, we should use the standard
    scheme for constructing SPIR-V version; and by doing that we can
    also map client API entries to universial SPIR-V versions.

--- source/opcode.cpp
+++ source/opcode.cpp
@@ -106,7 +106,7 @@ spv_result_t spvOpcodeTableNameLookup(spv_target_env env,
     // Note that the second rule assumes the extension enabling this instruction
     // is indeed requested in the SPIR-V code; checking that should be
     // validator's work.
-    if ((static_cast<uint32_t>(env) >= entry.minVersion ||
+    if ((spvVersionForTargetEnv(env) >= entry.minVersion ||
          entry.numExtensions > 0u) &&
         nameLength == strlen(entry.name) &&
         !strncmp(name, entry.name, nameLength)) {
@@ -151,7 +151,7 @@ spv_result_t spvOpcodeTableValueLookup(spv_target_env env,
     // Note that the second rule assumes the extension enabling this instruction
     // is indeed requested in the SPIR-V code; checking that should be
     // validator's work.
-    if (static_cast<uint32_t>(env) >= it->minVersion ||
+    if (spvVersionForTargetEnv(env) >= it->minVersion ||
         it->numExtensions > 0u) {
       *pEntry = it;
       return SPV_SUCCESS;
