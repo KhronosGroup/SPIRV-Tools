b7cf39ad0b08b2cf4acd785e921ab896ac475980
Steven Perron <stevenperron@google.com>
2018-04-12 22:55:12 -0400

    Add comments to the test.

--- test/util/bit_vector_test.cpp
+++ test/util/bit_vector_test.cpp
@@ -25,6 +25,9 @@ using BitVectorTest = ::testing::Test;
 
 TEST(BitVectorTest, Initialize) {
   BitVector bvec;
+
+  // Checks that all values are 0.  Also tests checking a  bit past the end of
+  // the vector containing the bits.
   for (int i = 1; i < 10000; i *= 2) {
     EXPECT_FALSE(bvec.Get(i));
   }
@@ -32,14 +35,19 @@ TEST(BitVectorTest, Initialize) {
 
 TEST(BitVectorTest, Set) {
   BitVector bvec;
+
+  // Since 10,000 is larger than the initial size, this tests the resizing
+  // code.
   for (int i = 3; i < 10000; i *= 2) {
     bvec.Set(i);
   }
 
+  // Check that bits that were not set are 0.
   for (int i = 1; i < 10000; i *= 2) {
     EXPECT_FALSE(bvec.Get(i));
   }
 
+  // Check that bits that were set are 1.
   for (int i = 3; i < 10000; i *= 2) {
     EXPECT_TRUE(bvec.Get(i));
   }
@@ -51,19 +59,20 @@ TEST(BitVectorTest, Clear) {
     bvec.Set(i);
   }
 
-  for (int i = 1; i < 10000; i *= 2) {
-    EXPECT_FALSE(bvec.Get(i));
-  }
-
+  // Check that the bits were properly set.
   for (int i = 3; i < 10000; i *= 2) {
     EXPECT_TRUE(bvec.Get(i));
   }
 
+  // Clear all of the bits except for bit 3.
   for (int i = 6; i < 10000; i *= 2) {
     bvec.Clear(i);
   }
 
+  // Make sure bit 3 was not cleared.
   EXPECT_TRUE(bvec.Get(3));
+
+  // Make sure all of the other bits that were set have been cleared.
   for (int i = 6; i < 10000; i *= 2) {
     EXPECT_FALSE(bvec.Get(i));
   }
