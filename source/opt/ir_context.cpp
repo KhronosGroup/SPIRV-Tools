2dc7cb9b13d31951652db5ccdd741a4f5555de43
Greg Roth <goyoroth@gmail.com>
2018-06-27 12:39:27 -0600

    fix assert during compact IDs pass
    During the compact IDs optimization pass, the result IDs of some
    basic blocks can change. In spite of this, GetPreservedAnalyses
    indicated that the CFG was preserved. But the config relies on
    the basic blocks having the same IDs. Simply removing this flag
    resolves the issue by preventing the config check.
    
    However, the config retrieval would update the config if this bit
    is set. So it's not necessary to skip the config test even when it
    has been changed. So I've removed that early exit too
    
    Addresses https://github.com/KhronosGroup/SPIRV-Tools/issues/1648

--- source/opt/ir_context.cpp
+++ source/opt/ir_context.cpp
@@ -601,9 +601,6 @@ opt::PostDominatorAnalysis* IRContext::GetPostDominatorAnalysis(
 
 bool ir::IRContext::CheckCFG() {
   std::unordered_map<uint32_t, std::vector<uint32_t>> real_preds;
-  if (!AreAnalysesValid(kAnalysisCFG)) {
-    return true;
-  }
 
   for (ir::Function& function : *module()) {
     for (const auto& bb : function) {
