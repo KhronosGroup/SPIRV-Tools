5304dc3b8471073e2195965e36228d09563f795e
Steven Perron <31666470+s-perron@users.noreply.github.com>
2018-08-27 23:45:32 -0400

    Validate all type ids. (#1868)
    * Validate all type ids.
    
    The validator does not check if the type of an instruction is actually
    a type unless the OpCode has a specific requirement.  For example,
    OpFAdd is checked, but OpUndef is not.
    
    The commit add a generic check that if there is a type id then the id
    defines a type.
    
    http://crbug.com/876694
    
    * Merge other checks for type into new one.
    
    There are a couple check that the type id is a type for specific
    opcodes.  Those have been mereged into 1.
    
    Small changes to other test cases to make them valid enough for the
    purpose of the test.

--- source/val/validate_id.cpp
+++ source/val/validate_id.cpp
@@ -162,13 +162,27 @@ spv_result_t IdPass(ValidationState_t& _, Instruction* inst) {
         ret = SPV_SUCCESS;
         break;
       case SPV_OPERAND_TYPE_ID:
-      case SPV_OPERAND_TYPE_TYPE_ID:
       case SPV_OPERAND_TYPE_MEMORY_SEMANTICS_ID:
       case SPV_OPERAND_TYPE_SCOPE_ID:
         if (_.IsDefinedId(operand_word)) {
           ret = SPV_SUCCESS;
         } else if (can_have_forward_declared_ids(i)) {
           ret = _.ForwardDeclareId(operand_word);
+        } else {
+          ret = _.diag(SPV_ERROR_INVALID_ID, inst)
+                << "ID " << _.getIdName(operand_word)
+                << " has not been defined";
+        }
+        break;
+      case SPV_OPERAND_TYPE_TYPE_ID:
+        if (_.IsDefinedId(operand_word)) {
+          auto* def = _.FindDef(operand_word);
+          if (!spvOpcodeGeneratesType(def->opcode())) {
+            ret = _.diag(SPV_ERROR_INVALID_ID, inst)
+                  << "ID " << _.getIdName(operand_word) << " is not a type id";
+          } else {
+            ret = SPV_SUCCESS;
+          }
         } else {
           ret = _.diag(SPV_ERROR_INVALID_ID, inst)
                 << "ID " << _.getIdName(operand_word)
