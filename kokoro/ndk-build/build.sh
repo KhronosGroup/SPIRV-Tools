a78fa0978d6415bc53465a5c075d291ba71df12b
alan-baker <33432579+alan-baker@users.noreply.github.com>
2018-09-27 09:44:01 -0400

    Set the C++ version to C++11 (#1933)
    This reverts commit bfcdc913c4184430e4fca6923683c62723bf5956.

--- kokoro/ndk-build/build.sh
+++ kokoro/ndk-build/build.sh
@@ -34,8 +34,7 @@ export ANDROID_NDK=/opt/android-ndk-r15c
 # Get the dependencies.
 cd $SRC
 git clone --depth=1 https://github.com/KhronosGroup/SPIRV-Headers external/spirv-headers
-git clone https://github.com/google/googletest          external/googletest
-cd external && cd googletest && git reset --hard 0fc5466dbb9e623029b1ada539717d10bd45e99e && cd .. && cd ..
+git clone --depth=1 https://github.com/google/googletest          external/googletest
 git clone --depth=1 https://github.com/google/effcee              external/effcee
 git clone --depth=1 https://github.com/google/re2                 external/re2
 
