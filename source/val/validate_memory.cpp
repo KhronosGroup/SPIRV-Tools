c681bdfc8eb1db64a2f1b6b124fe331e9df52bd3
Ryan Harrison <rharrison@google.com>
2018-11-22 10:07:54 -0500

    Rebase tests and add Vulkan check

--- source/val/validate_memory.cpp
+++ source/val/validate_memory.cpp
@@ -389,20 +389,32 @@ spv_result_t ValidateVariable(ValidationState_t& _, const Instruction* inst) {
     }
   }
 
-  // WebGPU: Check that if contains initializer, then storage class is Output,
-  // Private, or Function.
-  if (spvIsWebGPUEnv(_.context()->target_env) && inst->operands().size() > 3) {
-    if (storage_class != SpvStorageClassOutput &&
-        storage_class != SpvStorageClassPrivate &&
-        storage_class != SpvStorageClassFunction) {
+  // WebGPU & Vulkan Appendix A: Check that if contains initializer, then
+  // storage class is Output, Private, or Function.
+  if (inst->operands().size() > 3 &&
+      storage_class != SpvStorageClassOutput &&
+      storage_class != SpvStorageClassPrivate &&
+      storage_class != SpvStorageClassFunction) {
+    if (spvIsVulkanEnv(_.context()->target_env)) {
       return _.diag(SPV_ERROR_INVALID_ID, inst)
-             << "OpVariable, <id> '" << _.getIdName(inst->id())
-             << "', has a disallowed initializer & storage class "
-             << "combination.\n"
-             << "From WebGPU execution environment spec:\n"
-             << "Variable declarations that include initializers must have "
-             << "one of the following storage classes: Output, Private, or "
-             << "Function";
+          << "OpVariable, <id> '" << _.getIdName(inst->id())
+          << "', has a disallowed initializer & storage class "
+          << "combination.\n"
+          << "From Vulkan spec, Appendix A:\n"
+          << "Variable declarations that include initializers must have "
+          << "one of the following storage classes: Output, Private, or "
+          << "Function";
+    }
+
+    if (spvIsWebGPUEnv(_.context()->target_env)) {
+      return _.diag(SPV_ERROR_INVALID_ID, inst)
+          << "OpVariable, <id> '" << _.getIdName(inst->id())
+          << "', has a disallowed initializer & storage class "
+          << "combination.\n"
+          << "From WebGPU execution environment spec:\n"
+          << "Variable declarations that include initializers must have "
+          << "one of the following storage classes: Output, Private, or "
+          << "Function";
     }
   }
 
