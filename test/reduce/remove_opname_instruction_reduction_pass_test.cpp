71d9255805fd8bbd0e122b377ae5819e50d9ebee
Hugues Evrard <hevrard@google.com>
2018-12-07 13:47:29 +1100

    EnableRemoveUnreferencedInstruction

--- test/reduce/remove_opname_instruction_reduction_pass_test.cpp
+++ test/reduce/remove_opname_instruction_reduction_pass_test.cpp
@@ -17,6 +17,7 @@
 #include "source/opt/build_module.h"
 #include "source/reduce/reduction_opportunity.h"
 #include "source/reduce/remove_opname_instruction_reduction_pass.h"
+#include <source/reduce/remove_unreferenced_instruction_reduction_pass.h>
 
 namespace spvtools {
 namespace reduce {
@@ -128,27 +129,79 @@ TEST(RemoveOpnameInstructionReductionPassTest, TryApplyRemovesAllOpName) {
   const std::string expected = prologue + epilogue;
 
   const auto env = SPV_ENV_UNIVERSAL_1_3;
-
-  std::vector<uint32_t> binary;
-  SpirvTools t(env);
-  ASSERT_TRUE(t.Assemble(original, &binary, kReduceAssembleOption));
-
-  const auto consumer = nullptr;
-  const auto context =
-      BuildModule(env, consumer, original, kReduceAssembleOption);
   auto pass = TestSubclass<RemoveOpNameInstructionReductionPass>(env);
 
   {
     // Check the right number of opportunities is detected
+    const auto consumer = nullptr;
+    const auto context =
+        BuildModule(env, consumer, original, kReduceAssembleOption);
     const auto ops = pass.WrapGetAvailableOpportunities(context.get());
     ASSERT_EQ(5, ops.size());
   }
 
   {
     // The reduction should remove all OpName
+    std::vector<uint32_t> binary;
+    SpirvTools t(env);
+    ASSERT_TRUE(t.Assemble(original, &binary, kReduceAssembleOption));
     auto reduced_binary = pass.TryApplyReduction(binary);
     CheckEqual(env, expected, reduced_binary);
   }
+}
+
+TEST(RemoveOpnameInstructionReductionPassTest, EnableRemoveUnreferencedInstruction) {
+  const std::string source = R"(
+               OpCapability Shader
+          %1 = OpExtInstImport "GLSL.std.450"
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint Fragment %4 "main"
+               OpExecutionMode %4 OriginUpperLeft
+               OpSource ESSL 310
+               OpName %4 "main"
+               OpName %8 "a"
+               OpName %11 "this-name-counts-as-usage-for-load-instruction"
+          %2 = OpTypeVoid
+          %3 = OpTypeFunction %2
+          %6 = OpTypeFloat 32
+          %7 = OpTypePointer Function %6
+          %9 = OpConstant %6 1
+          %4 = OpFunction %2 None %3
+          %5 = OpLabel
+          %8 = OpVariable %7 Function
+         %11 = OpLoad %6 %8 ;; this OpLoad has no "use" outside of its OpName
+               OpReturn
+               OpFunctionEnd
+  )";
+
+  const auto consumer = nullptr;
+  const auto env = SPV_ENV_UNIVERSAL_1_3;
+  std::vector<uint32_t> binary;
+  SpirvTools t(env);
+  ASSERT_TRUE(t.Assemble(source, &binary, kReduceAssembleOption));
+
+  const auto unreferenced_inst_pass =
+      TestSubclass<RemoveUnreferencedInstructionReductionPass>(env);
+  auto opname_inst_pass =
+      TestSubclass<RemoveOpNameInstructionReductionPass>(env);
+
+  // Save unreferenced inst opportunities before applying OpName reduction
+  const auto context_before =
+      BuildModule(env, consumer, source, kReduceAssembleOption);
+  const auto unreferenced_inst_ops_before =
+      unreferenced_inst_pass.WrapGetAvailableOpportunities(context_before.get());
+
+  // Apply OpName reduction
+  auto reduced_binary = opname_inst_pass.TryApplyReduction(binary);
+
+  // Check that a new unreferenced inst opportunity has appeared
+  const auto context_after =
+      BuildModule(env, consumer, reduced_binary.data(), reduced_binary.size());
+  const auto unreferenced_inst_ops_after =
+      unreferenced_inst_pass.WrapGetAvailableOpportunities(context_after.get());
+  ASSERT_EQ(
+      unreferenced_inst_ops_after.size(),
+      unreferenced_inst_ops_before.size() + 1);
 }
 
 }  // namespace
