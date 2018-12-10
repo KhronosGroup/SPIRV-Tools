edfe6d69e9caff3f25d709588c7de7158d2a120b
Hugues Evrard <hevrard@users.noreply.github.com>
2018-12-11 03:53:31 +1100

    Add RemoveOpNameInstruction reduction pass (#2187)
    Add a spirv-reduce pass which removes OpName and OpMemberName instructions.
    
    This is useful to enable other reduction passes, e.g. RemoveUnreferencedInstruction may not be able to remove an instruction creating an id whose only usage is an OpName for this id.

--- tools/reduce/reduce.cpp
+++ tools/reduce/reduce.cpp
@@ -23,6 +23,7 @@
 #include "source/reduce/operand_to_const_reduction_pass.h"
 #include "source/reduce/operand_to_dominating_id_reduction_pass.h"
 #include "source/reduce/reducer.h"
+#include "source/reduce/remove_opname_instruction_reduction_pass.h"
 #include "source/reduce/remove_unreferenced_instruction_reduction_pass.h"
 #include "source/reduce/structured_loop_to_selection_reduction_pass.h"
 #include "source/spirv_reducer_options.h"
@@ -204,6 +205,8 @@ int main(int argc, const char** argv) {
         return ExecuteCommand(command);
       });
 
+  reducer.AddReductionPass(
+      spvtools::MakeUnique<RemoveOpNameInstructionReductionPass>(target_env));
   reducer.AddReductionPass(
       spvtools::MakeUnique<OperandToConstReductionPass>(target_env));
   reducer.AddReductionPass(
