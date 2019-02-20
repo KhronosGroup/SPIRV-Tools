5ebeac55cac8d74cc2bb9ed122abab73c4071818
SarahM0 <sarahmashay@google.com>
2019-02-20 15:29:03 -0500

    slow down invetigation

--- test/val/val_decoration_test.cpp
+++ test/val/val_decoration_test.cpp
@@ -1510,41 +1510,6 @@ TEST_F(ValidateDecorations, BlockLayoutPermitsTightVec3ScalarPackingGood) {
       << getDiagnosticString();
 }
 
-TEST_F(ValidateDecorations, BlockCantAppearWithinABlockBad) {
-  // See https://github.com/KhronosGroup/SPIRV-Tools/issues/1587
-  std::string spirv = R"(
-               OpCapability Shader
-               OpMemoryModel Logical GLSL450
-               OpEntryPoint Vertex %main "main"
-               OpSource GLSL 450
-               OpMemberDecorate %S 0 Offset 0
-               OpMemberDecorate %S 1 Offset 16
-               OpMemberDecorate %S2 0 Offset 0
-               OpMemberDecorate %S2 1 Offset 12
-               OpDecorate %S Block
-               OpDecorate %S2 Block
-               OpDecorate %B DescriptorSet 0
-               OpDecorate %B Binding 0
-       %void = OpTypeVoid
-          %3 = OpTypeFunction %void
-      %float = OpTypeFloat 32
-         %S2 = OpTypeStruct %float %float
-          %S = OpTypeStruct %float %S2
-%_ptr_Uniform_S = OpTypePointer Uniform %S
-          %B = OpVariable %_ptr_Uniform_S Uniform
-       %main = OpFunction %void None %3
-          %5 = OpLabel
-               OpReturn
-               OpFunctionEnd
-  )";
-
-  CompileSuccessfully(spirv);
-  EXPECT_EQ(SPV_ERROR_INVALID_ID, ValidateAndRetrieveValidationState());
-  EXPECT_THAT(getDiagnosticString(),
-              HasSubstr("rules: A Block/BufferBlock cannot appear within a "
-                        "Block/BufferBlock."));
-}
-
 TEST_F(ValidateDecorations, BufferblockCantAppearWithinABufferblockBad) {
   // See https://github.com/KhronosGroup/SPIRV-Tools/issues/1587
   std::string spirv = R"(
