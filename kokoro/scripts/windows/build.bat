1bb2c08126002c63ddea5930eb4fcaed1327dca4
Steven Perron <stevenperron@google.com>
2019-03-06 14:11:01 -0500

    Fix python scripts to work with python3 (#2426)
    Updated script to work with python3 and python2.
    
    Added required tools.
    
    We added a section to the readme to mention the tools that are needed to
    build and test spirv-tools. For the compiler, the compilers used by the
    bots are mentioned.
    
    The bots have been changed. The windows bots will not use python 3.6 for testing. The other bots will still use python 2.7. Both Python2 and Python3 will be tested.
    
    Fixes #2407.
    Fixes #1856.

--- kokoro/scripts/windows/build.bat
+++ kokoro/scripts/windows/build.bat
@@ -21,8 +21,8 @@ set SRC=%cd%\github\SPIRV-Tools
 set BUILD_TYPE=%1
 set VS_VERSION=%2
 
-:: Force usage of python 2.7 rather than 3.6
-set PATH=C:\python27;%PATH%
+:: Force usage of python 3.6
+set PATH=C:\python36;%PATH%
 
 cd %SRC%
 git clone --depth=1 https://github.com/KhronosGroup/SPIRV-Headers external/spirv-headers
