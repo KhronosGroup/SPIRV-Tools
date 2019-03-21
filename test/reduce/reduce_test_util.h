33c6c30e8638e0755a497ccc5b2d8bcba45b38c3
Paul Thomson <paulthomson@google.com>
2019-03-21 13:08:21 +0000

    Improve reducer algorithm and other changes
    * Add fail-on-validation-error option to fail a reduction if an invalid state is reached; useful for tests.
    * Set fail-on-validation-error in tests.
    * Improve some documentation comments.
    * Add Reducer::AddDefaultReductionPasses so tests (and other library consumers) can add the default reduction passes.
    * Add CLIMessageConsumer in test_reduce so we can see messages for tricky tests.
    * Remove test RemoveUnreferencedInstructionReductionPassTest_ApplyReduction because it was indirectly testing the reduction algorithm, not the RemoveUnreferencedInstruction pass.
    * Tweak tests where needed.

--- test/reduce/reduce_test_util.h
+++ test/reduce/reduce_test_util.h
@@ -59,6 +59,11 @@ const uint32_t kReduceDisassembleOption =
 void NopDiagnostic(spv_message_level_t /*level*/, const char* /*source*/,
                    const spv_position_t& /*position*/, const char* /*message*/);
 
+// Prints reducer messages (for debugging).
+void CLIMessageConsumer(spv_message_level_t level, const char*,
+                        const spv_position_t& position, const char* message);
+
+
 }  // namespace reduce
 }  // namespace spvtools
 
