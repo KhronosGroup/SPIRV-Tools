e453a0ada95a8d42424082098de387243a97e319
Alastair Donaldson <afdx@google.com>
2019-05-15 20:15:33 +0100

    Added suppression of -Wshadow when processing protobuf header files.

--- source/fuzz/protobufs/spirvfuzz_protobufs.h
+++ source/fuzz/protobufs/spirvfuzz_protobufs.h
@@ -24,6 +24,7 @@
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wshadow"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 
