88c04301404b6a9bafaa20577c8ec49a6872d9f7
Kevin Petit <kevin.petit@arm.com>
2019-05-16 15:53:26 +0100

    Fix copyright header
    Signed-off-by: Kevin Petit <kevin.petit@arm.com>

--- test/val/val_opencl_test.cpp
+++ test/val/val_opencl_test.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The SPIR-V Tools authors.
+// Copyright (c) 2019 The Khronos Group Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
