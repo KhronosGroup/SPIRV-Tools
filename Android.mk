d1e73c79a2a721c8544fa0288da9f04ff1054e7a
Frank Henigman <fjhenigman@chromium.org>
2019-06-04 21:45:46 -0400

    Fix rebase error.
    Remove file id_descriptor.cpp.

--- Android.mk
+++ Android.mk
@@ -13,7 +13,6 @@ SPVTOOLS_SRC_FILES := \
 		source/ext_inst.cpp \
 		source/enum_string_mapping.cpp \
 		source/extensions.cpp \
-		source/id_descriptor.cpp \
 		source/instructions.cpp \
 		source/libspirv.cpp \
 		source/name_mapper.cpp \
