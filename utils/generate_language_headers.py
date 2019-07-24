fe9d07472d5e3ad5c23667eed193f53d18b08578
Ryan Harrison <rharrison@google.com>
2019-07-24 14:46:49 -0400

    Fix even more print + open badness

--- utils/generate_language_headers.py
+++ utils/generate_language_headers.py
@@ -179,7 +179,8 @@ def main():
                                  version = grammar_json['version'],
                                  revision = grammar_json['revision'])
         make_path_to_file(args.extinst_output_base)
-        print(CGenerator().generate(grammar), file=open(args.extinst_output_base + '.h', 'w'))
+        with open(args.extinst_output_base + '.h', 'w') as f:
+            f.write(CGenerator().generate(grammar))
 
 
 if __name__ == '__main__':
