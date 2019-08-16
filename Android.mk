06407250a169c6a03b3765e86619075af1a8c187
greg-lunarg <greg@lunarg.com>
2019-08-16 07:18:34 -0600

    Instrument: Add support for Buffer Device Address extension (#2792)

--- Android.mk
+++ Android.mk
@@ -117,6 +117,7 @@ SPVTOOLS_OPT_SRC_FILES := \
 		source/opt/inline_exhaustive_pass.cpp \
 		source/opt/inline_opaque_pass.cpp \
 		source/opt/inst_bindless_check_pass.cpp \
+		source/opt/inst_buff_addr_check_pass.cpp \
 		source/opt/instruction.cpp \
 		source/opt/instruction_list.cpp \
 		source/opt/instrument_pass.cpp \
