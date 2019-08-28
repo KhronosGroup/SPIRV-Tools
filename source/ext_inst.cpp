6b03118195745a24dbed9b73528625b40224b6f4
David Neto <dneto@google.com>
2019-08-28 18:49:33 -0400

    Add basic support for Vulkan 1.2: SPV_ENV_VULKAN_1_2

--- source/ext_inst.cpp
+++ source/ext_inst.cpp
@@ -88,6 +88,7 @@ spv_result_t spvExtInstTableGet(spv_ext_inst_table* pExtInstTable,
     case SPV_ENV_WEBGPU_0:
     case SPV_ENV_UNIVERSAL_1_4:
     case SPV_ENV_UNIVERSAL_1_5:
+    case SPV_ENV_VULKAN_1_2:
       *pExtInstTable = &kTable_1_0;
       return SPV_SUCCESS;
     default:
