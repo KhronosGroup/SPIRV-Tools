66172c3e86a0d93ee3b9c381a19ffc2fb1c73111
Alastair Donaldson <afdx@google.com>
2019-10-10 13:34:38 +0100

    spirv-fuzz: Add fuzzer pass to change loop controls (#2949)
    A new pass that allows the fuzzer to change the 'loop control' operand
    (and associated literal operands) of OpLoopMerge instructions.
    
    Fixes #2938.
    Fixes #2943.

--- source/fuzz/fuzzer.cpp
+++ source/fuzz/fuzzer.cpp
@@ -23,6 +23,7 @@
 #include "source/fuzz/fuzzer_pass_add_dead_breaks.h"
 #include "source/fuzz/fuzzer_pass_add_dead_continues.h"
 #include "source/fuzz/fuzzer_pass_add_useful_constructs.h"
+#include "source/fuzz/fuzzer_pass_adjust_loop_controls.h"
 #include "source/fuzz/fuzzer_pass_adjust_selection_controls.h"
 #include "source/fuzz/fuzzer_pass_apply_id_synonyms.h"
 #include "source/fuzz/fuzzer_pass_construct_composites.h"
@@ -169,6 +170,11 @@ Fuzzer::FuzzerResultStatus Fuzzer::Run(
 
   // Now apply some passes that it does not make sense to apply repeatedly,
   // as they do not unlock other passes.
+  if (fuzzer_context.ChooseEven()) {
+    FuzzerPassAdjustLoopControls(ir_context.get(), &fact_manager,
+                                 &fuzzer_context, transformation_sequence_out)
+        .Apply();
+  }
   if (fuzzer_context.ChooseEven()) {
     FuzzerPassAdjustSelectionControls(ir_context.get(), &fact_manager,
                                       &fuzzer_context,
