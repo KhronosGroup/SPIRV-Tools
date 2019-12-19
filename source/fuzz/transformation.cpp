d6deb89e39dbe4035a700b04bf015a452233b483
Alastair Donaldson <afdx@google.com>
2019-12-19 14:44:40 +0000

    spirv-fuzz: Transformation to add a new function to a module
    This adds a large transformation that can add a new function to a
    SPIR-V module.  This paves the way for donation of code from one
    module to another.

--- source/fuzz/transformation.cpp
+++ source/fuzz/transformation.cpp
@@ -21,6 +21,7 @@
 #include "source/fuzz/transformation_add_constant_scalar.h"
 #include "source/fuzz/transformation_add_dead_break.h"
 #include "source/fuzz/transformation_add_dead_continue.h"
+#include "source/fuzz/transformation_add_function.h"
 #include "source/fuzz/transformation_add_global_undef.h"
 #include "source/fuzz/transformation_add_global_variable.h"
 #include "source/fuzz/transformation_add_no_contraction_decoration.h"
@@ -72,6 +73,8 @@ std::unique_ptr<Transformation> Transformation::FromMessage(
     case protobufs::Transformation::TransformationCase::kAddDeadContinue:
       return MakeUnique<TransformationAddDeadContinue>(
           message.add_dead_continue());
+    case protobufs::Transformation::TransformationCase::kAddFunction:
+      return MakeUnique<TransformationAddFunction>(message.add_function());
     case protobufs::Transformation::TransformationCase::kAddGlobalUndef:
       return MakeUnique<TransformationAddGlobalUndef>(
           message.add_global_undef());
