e1f23b3458e0f653ffac2dac83ff4f41274681bf
Vasyl Teliman <vasniktel@gmail.com>
2020-03-07 10:46:00 +0200

    Update copyright info

--- source/fuzz/transformation_permute_function_parameters.h
+++ source/fuzz/transformation_permute_function_parameters.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Google LLC
+// Copyright (c) 2020 Vasyl Teliman
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
