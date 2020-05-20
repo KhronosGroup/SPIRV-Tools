56b1b041fc18aa40ad118076fb4a37ac1a965b23
Jaebaek Seo <jaebaek@google.com>
2020-05-20 00:50:00 -0400

    Handle OpConstantNull in ssa-rewrite
    ssa-rewrite fails in `MemPass::GetPtr()` when the SPIR-V code contains
    `OpLoad` for the result id of `OpConstantNull` because of the out of
    index access for an operand to get the base address. This commit fixes
    it.

--- source/opt/mem_pass.cpp
+++ source/opt/mem_pass.cpp
@@ -97,6 +97,8 @@ Instruction* MemPass::GetPtr(uint32_t ptrId, uint32_t* varId) {
   Instruction* ptrInst = get_def_use_mgr()->GetDef(*varId);
   Instruction* varInst;
 
+  if (ptrInst->opcode() == SpvOpConstantNull) return ptrInst;
+
   if (ptrInst->opcode() != SpvOpVariable &&
       ptrInst->opcode() != SpvOpFunctionParameter) {
     varInst = ptrInst->GetBaseAddress();
