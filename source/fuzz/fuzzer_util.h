44b13a016797871ba90f771a0a2344fb76faf612
Vasyl Teliman <vasniktel@gmail.com>
2020-06-23 18:41:45 +0300

    spirv-fuzz: Add GetParameters (#3454)
    Adds a GetParameters function to fuzzerutil.

--- source/fuzz/fuzzer_util.h
+++ source/fuzz/fuzzer_util.h
@@ -261,6 +261,11 @@ bool HasDuplicates(const std::vector<uint32_t>& arr);
 bool IsPermutationOfRange(const std::vector<uint32_t>& arr, uint32_t lo,
                           uint32_t hi);
 
+// Returns OpFunctionParameter instructions corresponding to the function
+// with result id |function_id|.
+std::vector<opt::Instruction*> GetParameters(opt::IRContext* ir_context,
+                                             uint32_t function_id);
+
 }  // namespace fuzzerutil
 
 }  // namespace fuzz
