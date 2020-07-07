6deae747b74f6be8f2cd6cd7cee62d3a65b3a1c9
André Perez Maselco <andreperezmaselco.developer@gmail.com>
2020-07-07 15:13:02 -0300

    Add sample module comment to help understand the transformation

--- source/fuzz/fuzzer_pass_add_image_sample_unused_components.cpp
+++ source/fuzz/fuzzer_pass_add_image_sample_unused_components.cpp
@@ -35,7 +35,50 @@ FuzzerPassAddImageSampleUnusedComponents::
     ~FuzzerPassAddImageSampleUnusedComponents() = default;
 
 void FuzzerPassAddImageSampleUnusedComponents::Apply() {
+  // SPIR-V module to help understand the transformation.
+  //
+  //       OpCapability Shader
+  //  %1 = OpExtInstImport "GLSL.std.450"
+  //       OpMemoryModel Logical GLSL450
+  //       OpEntryPoint Fragment %15 "main" %12 %14
+  //       OpExecutionMode %15 OriginUpperLeft
+  //
+  // ; Decorations
+  //        OpDecorate %12 Location 0 ; Input color variable location
+  //        OpDecorate %13 DescriptorSet 0 ; Image coordinate variable
+  //        descriptor set OpDecorate %13 Binding 0 ; Image coordinate
+  //        variable binding OpDecorate %14 Location 0 ; Fragment color
+  //        variable location
+  //
+  // ; Types
+  //  %2 = OpTypeVoid
+  //  %3 = OpTypeFunction %2
+  //  %4 = OpTypeFloat 32
+  //  %5 = OpTypeVector %4 2
+  //  %6 = OpTypeVector %4 4
+  //  %7 = OpTypeImage %4 2D 0 0 0 1 Rgba32f
+  //  %8 = OpTypeSampledImage %7
+  //  %9 = OpTypePointer Input %5
+  // %10 = OpTypePointer UniformConstant %8
+  // %11 = OpTypePointer Output %6
+  //
+  // ; Variables
+  // %12 = OpVariable %9 Input ; Input image coordinate variable
+  // %13 = OpVariable %10 UniformConstant ; Image variable
+  // %14 = OpVariable %11 Output ; Fragment color variable
+  //
+  // ; main function
+  // %15 = OpFunction %2 None %3
+  // %16 = OpLabel
+  // %17 = OpLoad %5 %12
+  // %18 = OpLoad %8 %13
+  // %19 = OpImageSampleImplicitLod %6 %18 %17
+  //       OpStore %14 %19
+  //       OpReturn
+  //       OpFunctionEnd
+
   GetIRContext()->module()->ForEachInst([this](opt::Instruction* instruction) {
+    // |instruction| %19 = OpImageSampleImplicitLod %6 %18 %17
     if (!spvOpcodeIsImageSample(instruction->opcode())) {
       return;
     }
@@ -47,6 +90,7 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
     }
 
     // Gets image sample coordinate information.
+    // |coordinate_instruction| %17 = OpLoad %5 %12
     uint32_t coordinate_id = instruction->GetSingleWordInOperand(1);
     auto coordinate_instruction =
         GetIRContext()->get_def_use_mgr()->GetDef(coordinate_id);
@@ -63,10 +107,14 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
     // If the coordinate is a scalar, then at most 3 unused components may be
     // added. If the coordinate is a vector, then the maximum number of unused
     // components depends on the vector size.
+    // For the sample module, the coordinate type instruction is %5 =
+    // OpTypeVector %4 2, thus |max_unused_component_count| = 4 - 2 = 2.
     uint32_t max_unused_component_count =
         coordinate_type->AsInteger() || coordinate_type->AsFloat()
             ? 3
             : 4 - coordinate_type->AsVector()->element_count();
+
+    // |unused_component_count| may be 1 or 2.
     uint32_t unused_component_count =
         GetFuzzerContext()->GetRandomUnusedComponentCountForImageSample(
             max_unused_component_count);
@@ -91,6 +139,8 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
         // type is the same as the coordinate. If the coordinate is a vector,
         // then the unused components type is the same as the coordinate
         // components type.
+        // %20 = OpConstant %4 0
+        // %21 = OpConstantComposite %5 %20 %20
         zero_constant_type_id =
             coordinate_type->AsInteger() || coordinate_type->AsFloat()
                 ? FindOrCreateVectorType(coordinate_instruction->type_id(),
@@ -110,7 +160,7 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
         coordinate_instruction->type_id());
 
     // If the new vector type with unused components does not exist, then create
-    // it.
+    // it. |coordinate_with_unused_components_type_id| %6 = OpTypeVector %4 4
     uint32_t coordinate_with_unused_components_type_id =
         coordinate_type->AsInteger() || coordinate_type->AsFloat()
             ? FindOrCreateVectorType(coordinate_instruction->type_id(),
@@ -123,6 +173,8 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
 
     // Inserts an OpCompositeConstruct instruction which
     // represents the coordinate with unused components.
+    // |coordinate_with_unused_components_id|
+    // %22 = OpCompositeConstruct %6 %17 %21
     uint32_t coordinate_with_unused_components_id =
         GetFuzzerContext()->GetFreshId();
     ApplyTransformation(TransformationCompositeConstruct(
@@ -133,6 +185,7 @@ void FuzzerPassAddImageSampleUnusedComponents::Apply() {
         coordinate_with_unused_components_id));
 
     // Tries to add unused components to the image sample coordinate.
+    // %19 = OpImageSampleImplicitLod %6 %18 %22
     ApplyTransformation(TransformationAddImageSampleUnusedComponents(
         coordinate_with_unused_components_id,
         MakeInstructionDescriptor(GetIRContext(), instruction)));
