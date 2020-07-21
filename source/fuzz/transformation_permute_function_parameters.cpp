fe9e5db890a6f48e6068e03dd0c9381903478ef3
Vasyl Teliman <vasniktel@gmail.com>
2020-07-21 23:02:32 +0300

    spirv-fuzz: TransformationReplaceParamsWithStruct (#3455)
    Fixes #3453.

--- source/fuzz/transformation_permute_function_parameters.cpp
+++ source/fuzz/transformation_permute_function_parameters.cpp
@@ -91,29 +91,29 @@ void TransformationPermuteFunctionParameters::Apply(
       fuzzerutil::GetFunctionType(ir_context, function);
   assert(old_function_type_inst && "Function must have a valid type");
 
-  // Change function's type
-  if (ir_context->get_def_use_mgr()->NumUsers(old_function_type_inst) == 1) {
+  std::vector<uint32_t> type_ids = {
+      old_function_type_inst->GetSingleWordInOperand(0)};
+  for (auto index : message_.permutation()) {
+    // +1 since the first operand to OpTypeFunction is a return type.
+    type_ids.push_back(
+        old_function_type_inst->GetSingleWordInOperand(index + 1));
+  }
+
+  // Change function's type.
+  if (ir_context->get_def_use_mgr()->NumUsers(old_function_type_inst) == 1 &&
+      fuzzerutil::FindFunctionType(ir_context, type_ids) == 0) {
     // If only the current function uses |old_function_type_inst| - change it
-    // in-place.
-    opt::Instruction::OperandList permuted_operands = {
-        std::move(old_function_type_inst->GetInOperand(0))};
-    for (auto index : message_.permutation()) {
+    // in-place. We can only do that if the module doesn't contain
+    // a function type with the permuted order of operands.
+    opt::Instruction::OperandList permuted_operands;
+    for (auto id : type_ids) {
       // +1 since the first operand to OpTypeFunction is a return type.
-      permuted_operands.push_back(
-          std::move(old_function_type_inst->GetInOperand(index + 1)));
+      permuted_operands.push_back({SPV_OPERAND_TYPE_ID, {id}});
     }
 
     old_function_type_inst->SetInOperands(std::move(permuted_operands));
   } else {
     // Either use an existing type or create a new one.
-    std::vector<uint32_t> type_ids = {
-        old_function_type_inst->GetSingleWordInOperand(0)};
-    for (auto index : message_.permutation()) {
-      // +1 since the first operand to OpTypeFunction is a return type.
-      type_ids.push_back(
-          old_function_type_inst->GetSingleWordInOperand(index + 1));
-    }
-
     function->DefInst().SetInOperand(
         1, {fuzzerutil::FindOrCreateFunctionType(
                ir_context, message_.function_type_fresh_id(), type_ids)});
@@ -146,24 +146,18 @@ void TransformationPermuteFunctionParameters::Apply(
       });
 
   // Fix all OpFunctionCall instructions
-  ir_context->get_def_use_mgr()->ForEachUser(
-      &function->DefInst(), [this](opt::Instruction* call) {
-        if (call->opcode() != SpvOpFunctionCall ||
-            call->GetSingleWordInOperand(0) != message_.function_id()) {
-          return;
-        }
+  for (auto* call : fuzzerutil::GetCallers(ir_context, function->result_id())) {
+    opt::Instruction::OperandList call_operands = {
+        call->GetInOperand(0)  // Function id
+    };
 
-        opt::Instruction::OperandList call_operands = {
-            call->GetInOperand(0)  // Function id
-        };
+    for (auto index : message_.permutation()) {
+      // Take function id into account
+      call_operands.push_back(call->GetInOperand(index + 1));
+    }
 
-        for (auto index : message_.permutation()) {
-          // Take function id into account
-          call_operands.push_back(call->GetInOperand(index + 1));
-        }
-
-        call->SetInOperands(std::move(call_operands));
-      });
+    call->SetInOperands(std::move(call_operands));
+  }
 
   // Make sure our changes are analyzed
   ir_context->InvalidateAnalysesExceptFor(
