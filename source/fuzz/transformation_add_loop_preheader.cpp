661907bcfc44946c57997351c72dc93062ecb6fa
Stefano Milizia <stefanomil@google.com>
2020-07-27 12:48:30 +0200

    Write comments for methods of TransformationAddLoopPreheader and implement IsApplicable

--- source/fuzz/transformation_add_loop_preheader.cpp
+++ source/fuzz/transformation_add_loop_preheader.cpp
@@ -14,6 +14,9 @@
 
 #include "transformation_add_loop_preheader.h"
 
+#include "source/fuzz/fuzzer_util.h"
+#include "source/opt/instruction.h"
+
 namespace spvtools {
 namespace fuzz {
 TransformationAddLoopPreheader::TransformationAddLoopPreheader(
@@ -31,10 +34,49 @@ TransformationAddLoopPreheader::TransformationAddLoopPreheader(
 }
 
 bool TransformationAddLoopPreheader::IsApplicable(
-    opt::IRContext* /* ir_context */,
-    const TransformationContext& /* transformation_context */) const {
-  return false;
+    opt::IRContext* ir_context,
+    const TransformationContext& transformation_context) const {
+  // |message_.loop_header_block()| must be the id of a loop header block.
+  opt::BasicBlock* loop_header_block =
+      fuzzerutil::MaybeFindBlock(ir_context, message_.loop_header_block());
+  if (!loop_header_block || !loop_header_block->IsLoopHeader()) {
+    return false;
+  }
+
+  // The id for the preheader must actually be fresh.
+  std::set<uint32_t> used_ids;
+  if (!CheckIdIsFreshAndNotUsedByThisTransformation(message_.fresh_id(),
+                                                    ir_context, &used_ids)) {
+    return false;
+  }
+
+  // If the block only has one predecessor outside of the loop (and thus 2 in
+  // total), then no additional fresh ids are necessary.
+  if (ir_context->cfg()->preds(message_.fresh_id()).size() == 2) {
+    return true;
+  }
+
+  // Count the number of OpPhi instructions.
+  int32_t num_phi_insts = 0;
+  loop_header_block->ForEachPhiInst(
+      [&num_phi_insts](opt::Instruction* /* unused */) { num_phi_insts++; });
+
+  // There must be enough fresh ids for the OpPhi instructions.
+  if (num_phi_insts > message_.phi_id_size()) {
+    return false;
+  }
+
+  // Check that the needed ids are fresh and distinct.
+  for (int32_t i = 0; i < num_phi_insts; i++) {
+    if (!CheckIdIsFreshAndNotUsedByThisTransformation(message_.phi_id(i),
+                                                      ir_context, &used_ids)) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
 void TransformationAddLoopPreheader::Apply(
     opt::IRContext* /* ir_context */,
     TransformationContext* /* transformation_context */) const {}
--- source/fuzz/transformation_add_loop_preheader.h
+++ source/fuzz/transformation_add_loop_preheader.h
@@ -28,12 +28,20 @@ class TransformationAddLoopPreheader : public Transformation {
   TransformationAddLoopPreheader(uint32_t loop_header_block, uint32_t fresh_id,
                                  std::vector<uint32_t> phi_id);
 
-  // TODO: Comment
+  // - |message_.loop_header_block| must be the id of a loop header block in
+  //   the given module.
+  // - |message_.fresh_id| must be an available id.
+  // - |message_.phi_ids| must be a list of available ids.
+  //   It can be empty if the loop header only has one predecessor outside of
+  //   the loop. Otherwise, it must contain at least as many ids as OpPhi
+  //   instructions in the loop header block.
   bool IsApplicable(
       opt::IRContext* ir_context,
       const TransformationContext& transformation_context) const override;
 
-  // TODO: Comment
+  // Adds a preheader block as the unique out-of-loop predecessor of the given
+  // loop header block. All of the existing out-of-loop predecessors of the
+  // header are changed so that they branch to the preheader instead.
   void Apply(opt::IRContext* ir_context,
              TransformationContext* transformation_context) const override;
 
