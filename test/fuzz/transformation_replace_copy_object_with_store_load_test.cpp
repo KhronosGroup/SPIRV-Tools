8d2f7b5091633b8e9e76001ca4c737672480633e
Antoni Karpiński <antonikarp@google.com>
2020-08-12 18:18:56 +0000

    Fixes comment.

--- test/fuzz/transformation_replace_copy_object_with_store_load_test.cpp
+++ test/fuzz/transformation_replace_copy_object_with_store_load_test.cpp
@@ -108,7 +108,8 @@ TEST(TransformationReplaceCopyObjectWithStoreLoad, BasicScenarios) {
   ASSERT_FALSE(transformation_invalid_4.IsApplicable(context.get(),
                                                      transformation_context));
 
-  // Invalid: initializer_id=15 is invalid.
+  // Invalid: initializer_id=15 has the wrong type relative to the OpCopyObject
+  // instruction.
   auto transformation_invalid_5 = TransformationReplaceCopyObjectWithStoreLoad(
       27, 30, SpvStorageClassFunction, 15);
   ASSERT_FALSE(transformation_invalid_5.IsApplicable(context.get(),
