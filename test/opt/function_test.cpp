eb2fc0861cefac52cd08c347bbc298be43595b9e
Alan Baker <alanbaker@google.com>
2020-08-13 09:44:57 -0400

    Add tests

--- test/opt/function_test.cpp
+++ test/opt/function_test.cpp
@@ -168,6 +168,80 @@ OpFunctionEnd
   EXPECT_FALSE(func->IsRecursive());
 }
 
+TEST(FunctionTest, NonSemanticInfoSkipIteration) {
+  const std::string text = R"(
+OpCapability Shader
+OpCapability Linkage
+OpExtension "SPV_KHR_non_semantic_info"
+%1 = OpExtInstImport "NonSemantic.Test"
+OpMemoryModel Logical GLSL450
+%2 = OpTypeVoid
+%3 = OpTypeFunction %2
+%4 = OpFunction %2 None %3
+%5 = OpLabel
+%6 = OpExtInst %2 %1 1
+OpReturn
+OpFunctionEnd
+%7 = OpExtInst %2 %1 2
+%8 = OpExtInst %2 %1 3
+)";
+
+  std::unique_ptr<IRContext> ctx =
+      spvtools::BuildModule(SPV_ENV_UNIVERSAL_1_1, nullptr, text,
+                            SPV_TEXT_TO_BINARY_OPTION_PRESERVE_NUMERIC_IDS);
+  auto* func = spvtest::GetFunction(ctx->module(), 4);
+  ASSERT_TRUE(func != nullptr);
+  std::unordered_set<uint32_t> non_semantic_ids;
+  func->ForEachInst(
+      [&non_semantic_ids](const Instruction* inst) {
+        if (inst->opcode() == SpvOpExtInst) {
+          non_semantic_ids.insert(inst->result_id());
+        }
+      },
+      true, false);
+
+  EXPECT_EQ(1, non_semantic_ids.count(6));
+  EXPECT_EQ(0, non_semantic_ids.count(7));
+  EXPECT_EQ(0, non_semantic_ids.count(8));
+}
+
+TEST(FunctionTest, NonSemanticInfoIncludeIteration) {
+  const std::string text = R"(
+OpCapability Shader
+OpCapability Linkage
+OpExtension "SPV_KHR_non_semantic_info"
+%1 = OpExtInstImport "NonSemantic.Test"
+OpMemoryModel Logical GLSL450
+%2 = OpTypeVoid
+%3 = OpTypeFunction %2
+%4 = OpFunction %2 None %3
+%5 = OpLabel
+%6 = OpExtInst %2 %1 1
+OpReturn
+OpFunctionEnd
+%7 = OpExtInst %2 %1 2
+%8 = OpExtInst %2 %1 3
+)";
+
+  std::unique_ptr<IRContext> ctx =
+      spvtools::BuildModule(SPV_ENV_UNIVERSAL_1_1, nullptr, text,
+                            SPV_TEXT_TO_BINARY_OPTION_PRESERVE_NUMERIC_IDS);
+  auto* func = spvtest::GetFunction(ctx->module(), 4);
+  ASSERT_TRUE(func != nullptr);
+  std::unordered_set<uint32_t> non_semantic_ids;
+  func->ForEachInst(
+      [&non_semantic_ids](const Instruction* inst) {
+        if (inst->opcode() == SpvOpExtInst) {
+          non_semantic_ids.insert(inst->result_id());
+        }
+      },
+      true, true);
+
+  EXPECT_EQ(1, non_semantic_ids.count(6));
+  EXPECT_EQ(1, non_semantic_ids.count(7));
+  EXPECT_EQ(1, non_semantic_ids.count(8));
+}
+
 }  // namespace
 }  // namespace opt
 }  // namespace spvtools
