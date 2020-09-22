5a5b750aaf88c30f8feb1ec02b055778bc975b7b
Vasyl Teliman <vasniktel@gmail.com>
2020-09-22 23:35:56 +0300

    spirv-fuzz: Handle OpPhis in TransformationInlineFunction (#3833)
    Fixes #3829.

--- test/fuzz/transformation_inline_function_test.cpp
+++ test/fuzz/transformation_inline_function_test.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "source/fuzz/transformation_inline_function.h"
+
 #include "source/fuzz/instruction_descriptor.h"
 #include "test/fuzz/fuzz_test_util.h"
 
@@ -749,6 +750,81 @@ TEST(TransformationInlineFunctionTest, ApplyToMultipleFunctions) {
 
   ASSERT_TRUE(IsValid(env, context.get()));
   ASSERT_TRUE(IsEqual(env, variant_shader, context.get()));
+}
+
+TEST(TransformationInlineFunctionTest, HandlesOpPhisInTheSecondBlock) {
+  std::string shader = R"(
+               OpCapability Shader
+          %1 = OpExtInstImport "GLSL.std.450"
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint Fragment %4 "main"
+               OpExecutionMode %4 OriginUpperLeft
+          %2 = OpTypeVoid
+          %3 = OpTypeFunction %2
+         %10 = OpTypeInt 32 0
+         %11 = OpUndef %10
+          %4 = OpFunction %2 None %3
+          %5 = OpLabel
+          %6 = OpFunctionCall %2 %7
+               OpBranch %14
+         %14 = OpLabel
+               OpReturn
+               OpFunctionEnd
+          %7 = OpFunction %2 None %3
+          %8 = OpLabel
+               OpBranch %13
+         %13 = OpLabel
+         %12 = OpPhi %10 %11 %8
+               OpReturn
+               OpFunctionEnd
+  )";
+
+  const auto env = SPV_ENV_UNIVERSAL_1_3;
+  const auto consumer = nullptr;
+  const auto context = BuildModule(env, consumer, shader, kFuzzAssembleOption);
+  ASSERT_TRUE(IsValid(env, context.get()));
+
+  FactManager fact_manager;
+  spvtools::ValidatorOptions validator_options;
+  TransformationContext transformation_context(&fact_manager,
+                                               validator_options);
+
+  TransformationInlineFunction transformation(6,
+                                              {{{8, 20}, {13, 21}, {12, 22}}});
+  ASSERT_TRUE(
+      transformation.IsApplicable(context.get(), transformation_context));
+  transformation.Apply(context.get(), &transformation_context);
+  ASSERT_TRUE(IsValid(env, context.get()));
+
+  std::string after_transformation = R"(
+               OpCapability Shader
+          %1 = OpExtInstImport "GLSL.std.450"
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint Fragment %4 "main"
+               OpExecutionMode %4 OriginUpperLeft
+          %2 = OpTypeVoid
+          %3 = OpTypeFunction %2
+         %10 = OpTypeInt 32 0
+         %11 = OpUndef %10
+          %4 = OpFunction %2 None %3
+          %5 = OpLabel
+               OpBranch %21
+         %21 = OpLabel
+         %22 = OpPhi %10 %11 %5
+               OpBranch %14
+         %14 = OpLabel
+               OpReturn
+               OpFunctionEnd
+          %7 = OpFunction %2 None %3
+          %8 = OpLabel
+               OpBranch %13
+         %13 = OpLabel
+         %12 = OpPhi %10 %11 %8
+               OpReturn
+               OpFunctionEnd
+  )";
+
+  ASSERT_TRUE(IsEqual(env, after_transformation, context.get()));
 }
 
 }  // namespace
