4ec3a3f1282d490a62c9a2d3cb7ededc66b96d49
Alastair Donaldson <afdx@google.com>
2020-10-13 10:30:51 +0100

    Small fix.

--- test/fuzz/transformation_duplicate_region_with_selection_test.cpp
+++ test/fuzz/transformation_duplicate_region_with_selection_test.cpp
@@ -1871,6 +1871,8 @@ TEST(TransformationDuplicateRegionWithSelectionTest, OverflowIds) {
 
 TEST(TransformationDuplicateRegionWithSelectionTest,
      RegionExitIsOpBranchConditional) {
+  // Checks that the transformation works when the exit block of a region ends
+  // with OpBranchConditional (but is not a header).
   std::string shader = R"(
                OpCapability Shader
           %1 = OpExtInstImport "GLSL.std.450"
