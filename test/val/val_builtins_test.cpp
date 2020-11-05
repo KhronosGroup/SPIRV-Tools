a61d07a72763c1eb200de0a2c316703643a0d1d9
sfricke-samsung <46493288+sfricke-samsung@users.noreply.github.com>
2020-11-05 10:04:47 -0800

    spirv-val: Fix SPV_KHR_fragment_shading_rate VUID label (#4014)
    in 3943 the new tests should have failed, but didn't due to the AnyVUID
    check not handling case with trailing whitespace. Added trimming to handle
    it as easily might happen again in future

--- test/val/val_builtins_test.cpp
+++ test/val/val_builtins_test.cpp
@@ -168,6 +168,17 @@ MATCHER_P(AnyVUID, vuid_set, "VUID from the set is in error message") {
   std::string token;
   std::string vuids = std::string(vuid_set);
   size_t position;
+
+  // Catch case were someone accidentally left spaces by trimming string
+  // clang-format off
+  vuids.erase(std::find_if(vuids.rbegin(), vuids.rend(), [](unsigned char c) {
+    return (c != ' ');
+  }).base(), vuids.end());
+  vuids.erase(vuids.begin(), std::find_if(vuids.begin(), vuids.end(), [](unsigned char c) {
+    return (c != ' ');
+  }));
+  // clang-format on
+
   do {
     position = vuids.find(delimiter);
     if (position != std::string::npos) {
@@ -3838,7 +3849,7 @@ INSTANTIATE_TEST_SUITE_P(
         Values("PrimitiveShadingRateKHR"), Values("Vertex"), Values("Output"),
         Values("%f32"), Values("OpCapability FragmentShadingRateKHR\n"),
         Values("OpExtension \"SPV_KHR_fragment_shading_rate\"\n"),
-        Values("VUID-PrimitiveShadingRateKHR-PrimitiveShadingRateKHR-04485 "),
+        Values("VUID-PrimitiveShadingRateKHR-PrimitiveShadingRateKHR-04486 "),
         Values(TestResult(
             SPV_ERROR_INVALID_DATA,
             "According to the Vulkan spec BuiltIn PrimitiveShadingRateKHR "
