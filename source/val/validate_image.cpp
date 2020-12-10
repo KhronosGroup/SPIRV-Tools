7170218b8deb9f6a0eade971159d1b8cbb44cacf
David Neto <dneto@google.com>
2020-12-10 10:40:46 -0500

    validate StorageImageMultisampled capability (#4062)
    * validate StorageImageMultisampled capability
    
    The StorageImageMultisampled capability is required when declaring
    an image type with with Multisampled==1 and it's a storage image (Sampled == 2)
    
    Fixes #4061
    
    * Allow SubpassData with Multisampled and Sampled==2

--- source/val/validate_image.cpp
+++ source/val/validate_image.cpp
@@ -829,6 +829,15 @@ spv_result_t ValidateTypeImage(ValidationState_t& _, const Instruction* inst) {
     }
   }
 
+  if (info.multisampled && (info.sampled == 2) &&
+      (info.dim != SpvDimSubpassData)) {
+    if (!_.HasCapability(SpvCapabilityStorageImageMultisample)) {
+      return _.diag(SPV_ERROR_INVALID_DATA, inst)
+             << "Capability StorageImageMultisample is required when using "
+                "multisampled storage image";
+    }
+  }
+
   return SPV_SUCCESS;
 }
 
