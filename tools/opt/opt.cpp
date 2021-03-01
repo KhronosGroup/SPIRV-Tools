b9a7a0b55ad911c5f2a945496e9312b366dc626e
Steven Perron <stevenperron@google.com>
2021-03-01 09:38:49 -0500

    Use standard function to get stdin to binary mode. (#4141)
    * Use standard function to get stdin to binary mode.
    
    The fisrt implementation to try to change stdin to binary mode is not
    protable.  Using freopen has a lot of implementation defined behaviour,
    and the code relied on that working.
    
    Looking into the MSVC documentation, I do not see a protable way of
    doing this.  I've implemented a Windows specific method and do nothing
    for other systems.  In general, UNIX based systems do make a distinction
    between text and binary files, so nothing needs to be done for them.
    
    Fixes #2518
    
    * Split ReadFile into two different functions.
    
    We want to remove the flag parameter.  So the original function has been
    replaced with ReadBinaryFile and ReadTextFile.  This should make the
    code more readable.
    
    * Change name of CorrectlyReadFile to avoid confusion.

--- tools/opt/opt.cpp
+++ tools/opt/opt.cpp
@@ -807,7 +807,7 @@ int main(int argc, const char** argv) {
   }
 
   std::vector<uint32_t> binary;
-  if (!ReadFile<uint32_t>(in_file, "rb", &binary)) {
+  if (!ReadBinaryFile<uint32_t>(in_file, &binary)) {
     return 1;
   }
 
