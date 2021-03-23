3423dccd0dd9e8975b6ccf46084286296bb70dd3
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-03-23 09:10:32 +0200

    add overrided methods into header

--- source/fuzz/transformation_swap_function_variables.h
+++ source/fuzz/transformation_swap_function_variables.h
@@ -14,6 +14,26 @@ namespace fuzz {
 
 class TransformationSwapFunctionVariables : public Transformation
 {
+    public:
+    explicit TransformationSwapFunctionVariables(
+      const protobufs::TransformationSwapFunctionVariables& message);
+
+    TransformationSwapFunctionVariables(uint32_t var_id1,
+                                    uint32_t var_id2);
+
+    bool IsApplicable(
+      opt::IRContext* ir_context,
+      const TransformationContext& transformation_context) const override;
+
+    void Apply(opt::IRContext* ir_context,
+             TransformationContext* transformation_context) const override;
+
+    protobufs::Transformation ToMessage() const override;
+    
+    std::unordered_set<uint32_t> GetFreshIds() const override;
+    
+    private:
+    protobufs::TransformationSwapFunctionVariables message_;
 
 };
 
