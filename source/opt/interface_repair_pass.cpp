64866857dbe4e1b0b258496b5e7a662f474ad413
ZHOU He <461607353@qq.com>
2021-05-13 17:48:58 +0800

    Refactor for readability.

--- source/opt/interface_repair_pass.cpp
+++ source/opt/interface_repair_pass.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Google LLC.
+// Copyright (c) 2021 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,60 +16,74 @@
 #include "source/spirv_constant.h"
 namespace spvtools {
 namespace opt {
+
+class EntryRepairContext {
+  InterfaceRepairPass& parent_;
+  Instruction& entry_;
+  std::unordered_set<uint32_t> used_variables_;
+  IRContext::ProcessFunction pfn_ = std::bind(
+      &EntryRepairContext::processFunction, this, std::placeholders::_1);
+
+  bool processFunction(Function* func) {
+    for (const auto& basicBlock : *func)
+      for (const auto& instruction : basicBlock)
+        for (const auto& operand : instruction)
+          if (operand.type == SPV_OPERAND_TYPE_ID) {
+            auto id = operand.words[0];
+            if (used_variables_.count(id)) continue;
+            auto* var = parent_.get_def_use_mgr()->GetDef(id);
+            if (!var || var->opcode() != SpvOpVariable) continue;
+            auto storage_class = var->GetSingleWordInOperand(0);
+            if (storage_class != SpvStorageClassFunction &&
+                (parent_.get_module()->version() >=
+                     SPV_SPIRV_VERSION_WORD(1, 4) ||
+                 storage_class == SpvStorageClassInput ||
+                 storage_class == SpvStorageClassOutput))
+              used_variables_.insert(id);
+          }
+    return false;
+  }
+
+ public:
+  EntryRepairContext(InterfaceRepairPass& parent, Instruction& entry)
+      : parent_(parent), entry_(entry) {}
+
+  void collectUsedVariables() {
+    std::queue<uint32_t> roots;
+    roots.push(entry_.result_id());
+    parent_.context()->ProcessCallTreeFromRoots(pfn_, &roots);
+  }
+
+  bool shouldRepair() {
+    std::unordered_set<uint32_t> old_variables;
+    for (int i = entry_.NumInOperands() - 1; i >= 3; --i) {
+      auto variable = entry_.GetInOperand(i).words[0];
+      if (!used_variables_.count(variable)) return true;  // It is unused.
+      if (old_variables.count(variable)) return true;     // It is duplicate.
+      old_variables.insert(variable);
+    }
+    if (old_variables.size() != used_variables_.size())  // Missing IDs.
+      return true;
+    return false;
+  }
+
+  void repair() {
+    for (int i = entry_.NumInOperands() - 1; i >= 3; --i)
+      entry_.RemoveInOperand(i);
+    for (auto id : used_variables_) {
+      entry_.AddOperand(Operand(SPV_OPERAND_TYPE_ID, {id}));
+    }
+  }
+};
+
 InterfaceRepairPass::Status InterfaceRepairPass::Process() {
-  std::unordered_map<uint32_t, Function*> id2function_;
-  for (auto& fn : *get_module()) id2function_[fn.result_id()] = &fn;
   bool modified = false;
   for (auto& entry : get_module()->entry_points()) {
-    std::set<uint32_t> used_variables;
-    std::function<void(Function * func)> traverser = [&](Function* func) {
-      for (auto bi = func->begin(); bi != func->end(); ++bi) {
-        for (auto ii = bi->begin(); ii != bi->end(); ++ii) {
-          if (ii->opcode() == SpvOpFunctionCall) {
-            const auto called_id = ii->GetOperand(2).words[0];
-            traverser(id2function_.at(called_id));
-          } else {
-            for (uint32_t i = 0; i < ii->NumOperands(); ++i) {
-              auto& op = ii->GetOperand(i);
-              if (op.type == SPV_OPERAND_TYPE_ID) {
-                auto id = op.words[0];
-                if (used_variables.count(id)) continue;
-                auto* var = get_def_use_mgr()->GetDef(id);
-                if (!var || var->opcode() != SpvOpVariable) continue;
-                auto storage_class = var->GetSingleWordInOperand(0);
-                if (storage_class != SpvStorageClassFunction &&
-                    (get_module()->version() >= SPV_SPIRV_VERSION_WORD(1, 4) ||
-                     storage_class == SpvStorageClassInput ||
-                     storage_class == SpvStorageClassOutput)) {
-                  used_variables.insert(id);
-                }
-              }
-            }
-          }
-        }
-      }
-    };
-    traverser(id2function_.at(entry.GetSingleWordInOperand(1)));
-    {
-      std::set<uint32_t> old_variables;
-      for (int i = entry.NumInOperands() - 1; i >= 3; --i) {
-        auto variable = entry.GetInOperand(i).words[0];
-        if (!used_variables.count(variable))  // There is unused ids.
-          goto repair;
-        if (old_variables.count(variable))  // There is duplicate ids.
-          goto repair;
-        old_variables.insert(variable);
-      }
-      if (old_variables.size() != used_variables.size())  // There is missing.
-        goto repair;
-    }
-    continue;
-  repair:
-    modified = true;
-    for (int i = entry.NumInOperands() - 1; i >= 3; --i)
-      entry.RemoveInOperand(i);
-    for (auto id : used_variables) {
-      entry.AddOperand(Operand(SPV_OPERAND_TYPE_ID, {id}));
+    EntryRepairContext context(*this, entry);
+    context.collectUsedVariables();
+    if (context.shouldRepair()) {
+      context.repair();
+      modified = true;
     }
   }
   return (modified ? Status::SuccessWithChange : Status::SuccessWithoutChange);
