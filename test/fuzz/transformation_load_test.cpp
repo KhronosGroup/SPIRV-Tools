ba40d02c5ab115fbfe8abadd9dbdc87287091e30
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-06-18 07:45:59 +0200

    Initial demo test case.

--- test/fuzz/transformation_load_test.cpp
+++ test/fuzz/transformation_load_test.cpp
@@ -289,6 +289,176 @@ TEST(TransformationLoadTest, BasicTest) {
   ASSERT_TRUE(IsEqual(env, after_transformation, context.get()));
 }
 
+TEST(TransformationLoadTest, DemoTestCase) {
+  /* (will remove this after PR ready)
+  I want to edit 'TransformationLoadTest' to work for both OpLoad and
+  OpAtomicLoad
+  ---------------------------------------------------------------
+  OpLoad:
+  %res_type_id = opCode(OpLoad) %res_id %ptr_id MemOperand(literal)(optional)
+  ----------------------------------------------------------------
+  OpAtomicLoad:
+  %res_type_id = opCode(OpAtomicLoad) %res_id %ptr_id %scope_id
+  %semantic_id(mask - 32-bitIntegerScalar)
+
+  load through Pointer using give semantics.
+
+  - **ptr_id** for atomic load must be one of this:
+  Uniform when, StorageBufferm, PhysicalStorageBuffer,
+  Workgroup, CrossWorkgroup, Generic AtomicCounter, Image, Function.
+  Type of result type must be the same of type the pointer point to.
+
+  - %scope_id is the define the scope of execution, (32-bit integer scalar)
+  If labeled as a memory scope,
+  it specifies the distance of synchronization from the current invocation, what
+  this means?
+  Scopes:
+    -Everything executing on all the execution devices in the system.
+    CrossDevice = 0
+    -Everything executing on the device executing this invocation
+    Device = 1
+    -All invocations for the invoking workgroup.
+    Workgroup = 2
+    -All invocations in the currently executing subgroup.
+    Subgroup = 3
+    - There is also
+    QueueFamily(current queue family), QueueFamilyKHR, ShaderCallKHR
+
+  - %res_id (obey SSA)
+
+  - %semantic_id(mask - const 32-bit integer scalar, can be combination)
+  Control Masks define bitmask hints for function optimisations.
+  */
+  std::string shader = R"(
+               OpCapability Shader
+          %1 = OpExtInstImport "GLSL.std.450"
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint Fragment %4 "main"
+               OpExecutionMode %4 OriginUpperLeft
+               OpSource ESSL 310
+          %2 = OpTypeVoid
+          %3 = OpTypeFunction %2
+          %6 = OpTypeInt 32 1
+          %7 = OpTypeInt 32 0 ; 0 means unsigned
+          %8 = OpConstant %7 0
+          %9 = OpTypePointer Function %6
+         %10 = OpTypeFunction %2
+         %11 = OpVariable %12 Workgroup
+         %12 = OpTypePointer Workgroup %6
+         %14 = OpConstant %6 0
+         %15 = OpTypePointer Function %6
+         %51 = OpTypePointer Private %6
+         %20 = OpVariable %9 Function
+         %21 = OpConstant %6 2
+         %23 = OpConstant %6 1
+         %25 = OpTypePointer Function %7
+         %50 = OpTypePointer Private %7
+         %34 = OpTypeBool
+         %35 = OpConstantFalse %34
+         %52 = OpVariable %50 Private
+         %53 = OpVariable %51 Private
+          %4 = OpFunction %2 None %3
+          %5 = OpLabel
+         %26 = OpAccessChain %25 %20 %23
+         %30 = OpAccessChain %15 %20 %14
+         %33 = OpAccessChain %15 %20 %14
+               OpSelectionMerge %37 None
+               OpBranchConditional %35 %36 %37
+         %36 = OpLabel
+         %38 = OpAccessChain %15 %20 %14
+         %40 = OpAccessChain %15 %20 %14
+         %43 = OpAccessChain %15 %20 %14
+               OpBranch %37
+         %37 = OpLabel
+               OpReturn
+               OpFunctionEnd
+  )";
+
+  // Making needed instructions
+  const auto env = SPV_ENV_UNIVERSAL_1_4;
+  const auto consumer = nullptr;
+  const auto context = BuildModule(env, consumer, shader, kFuzzAssembleOption);
+  spvtools::ValidatorOptions validator_options;
+  ASSERT_TRUE(fuzzerutil::IsValidAndWellFormed(context.get(), validator_options,
+                                               kConsoleMessageConsumer));
+  TransformationContext transformation_context(
+      MakeUnique<FactManager>(context.get()), validator_options);
+
+  // Expected (new)transformation after modification (New Args).
+  /*
+
+  TransformationLoad_NEW(
+      uint32_t fresh_id,
+      uint32_t pointer_id,
+      uint32_t execution_scope = 0 (default to avoid crash when working with OpLoad)
+      uint32_t memory_semantic = 0 (default to avoid crash when working with OpLoad)
+      bool opcode_flag *Flag will be false for OpLoad, true for OpAtomicLoad (Is this right?) *
+      const protobufs::InstructionDescriptor& instruction_to_insert_before);
+
+      - opcode_flag: will manage each OpCode
+      e.g.
+      if opcode_flag = false: will call something like OpLoad_processing(....) in IsApplicable or Apply.
+        TransformationLoad_NEW transformation_new (
+         60,
+         11,
+         2,
+         0x100,
+         true,
+         MakeInstructionDescriptor(38, OpAccessChain, 0));
+
+    *******Also will update protobufs*****
+  */
+
+  // Expected output After modifying transformation.
+  std::string after_transformation = R"(
+               OpCapability Shader
+          %1 = OpExtInstImport "GLSL.std.450"
+               OpMemoryModel Logical GLSL450
+               OpEntryPoint Fragment %4 "main"
+               OpExecutionMode %4 OriginUpperLeft
+               OpSource ESSL 310
+          %2 = OpTypeVoid
+          %3 = OpTypeFunction %2
+          %6 = OpTypeInt 32 1
+          %7 = OpTypeInt 32 0 ; 0 means unsigned
+          %8 = OpConstant %7 0
+          %9 = OpTypePointer Function %6
+         %10 = OpTypeFunction %2
+         %11 = OpVariable %12 Workgroup
+         %12 = OpTypePointer Workgroup %6
+         %14 = OpConstant %6 0
+         %15 = OpTypePointer Function %6
+         %51 = OpTypePointer Private %6
+         %20 = OpVariable %9 Function
+         %21 = OpConstant %6 2
+         %23 = OpConstant %6 1
+         %25 = OpTypePointer Function %7
+         %50 = OpTypePointer Private %7
+         %34 = OpTypeBool
+         %35 = OpConstantFalse %34
+         %52 = OpVariable %50 Private
+         %53 = OpVariable %51 Private
+          %4 = OpFunction %2 None %3
+          %5 = OpLabel
+         %26 = OpAccessChain %25 %20 %23
+         %30 = OpAccessChain %15 %20 %14
+         %33 = OpAccessChain %15 %20 %14
+               OpSelectionMerge %37 None
+               OpBranchConditional %35 %36 %37
+         %36 = OpLabel
+         %60 = OpAtomicLoad %6 %11 %8 %8
+         %38 = OpAccessChain %15 %20 %14
+         %40 = OpAccessChain %15 %20 %14
+         %43 = OpAccessChain %15 %20 %14
+               OpBranch %37
+         %37 = OpLabel
+               OpReturn
+               OpFunctionEnd
+  )";
+
+  //   ASSERT_TRUE(IsEqual(env, after_transformation, context.get()));
+}
+
 }  // namespace
 }  // namespace fuzz
 }  // namespace spvtools
