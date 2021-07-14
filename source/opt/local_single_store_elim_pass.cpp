4baf3affe32392b26b2b3394d9e79095d7c6d99a
Jaebaek Seo <jaebaek@google.com>
2021-07-14 08:43:35 -0400

    spirv-opt: support SPV_EXT_shader_image_int64 (#4379)

--- source/opt/local_single_store_elim_pass.cpp
+++ source/opt/local_single_store_elim_pass.cpp
@@ -125,6 +125,7 @@ void LocalSingleStoreElimPass::InitExtensionAllowList() {
       "SPV_KHR_terminate_invocation",
       "SPV_KHR_subgroup_uniform_control_flow",
       "SPV_KHR_integer_dot_product",
+      "SPV_EXT_shader_image_int64",
   });
 }
 bool LocalSingleStoreElimPass::ProcessVariable(Instruction* var_inst) {
