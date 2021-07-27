5404d572b6a3f83c9dc19cdba1bace184fdf75cc
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2021-07-27 18:05:11 +0200

    Addressed changes.

--- source/fuzz/transformation_load.cpp
+++ source/fuzz/transformation_load.cpp
@@ -31,8 +31,8 @@ TransformationLoad::TransformationLoad(
   message_.set_fresh_id(fresh_id);
   message_.set_pointer_id(pointer_id);
   message_.set_is_atomic(is_atomic);
-  message_.set_memory_scope(memory_scope);
-  message_.set_memory_semantics(memory_semantics);
+  message_.set_memory_scope_id(memory_scope);
+  message_.set_memory_semantics_id(memory_semantics);
 
   *message_.mutable_instruction_to_insert_before() =
       instruction_to_insert_before;
@@ -62,6 +62,53 @@ bool TransformationLoad::IsApplicable(
   switch (pointer->opcode()) {
     case SpvOpConstantNull:
     case SpvOpUndef:
+      return false;
+    default:
+      break;
+  }
+
+  // Check the exists of memory scope and memory semantics ids.
+  auto memory_scope_instruction =
+      ir_context->get_def_use_mgr()->GetDef(message_.memory_scope_id());
+  auto memory_semantics_instruction =
+      ir_context->get_def_use_mgr()->GetDef(message_.memory_semantics_id());
+
+  if (message_.is_atomic() && !memory_scope_instruction &&
+      !memory_semantics_instruction) {
+    return false;
+  }
+  // The memory scope and memory semantics instructions must have the
+  // 'OpConstant' opcode.
+  if (memory_scope_instruction->opcode() != SpvOpConstant &&
+      memory_semantics_instruction->opcode() != SpvOpConstant) {
+    return false;
+  }
+
+  // The memory scope and memory semantics instructions must have an Integer
+  // operand type with signedness matters.
+  if (!ir_context->get_type_mgr()
+           ->GetType(memory_scope_instruction->type_id())
+           ->AsInteger() &&
+      !ir_context->get_type_mgr()
+           ->GetType(memory_semantics_instruction->type_id())
+           ->AsInteger()) {
+    return false;
+  }
+
+  // The memory scope constant value must be that of SpvScopeInvocation.
+  auto memory_scope_const_value = memory_scope_instruction->GetInOperand(0);
+  if (memory_scope_const_value.words[0] != SpvScopeInvocation) {
+    return false;
+  }
+
+  // The memory semantics constant value must be either
+  // SpvMemorySemanticsWorkgroupMemoryMask or
+  // SpvMemorySemanticsUniformMemoryMask.
+  auto memory_semantics_const_value =
+      memory_semantics_instruction->GetInOperand(0);
+  switch (memory_semantics_const_value.words[0]) {
+    case SpvMemorySemanticsWorkgroupMemoryMask:
+    case SpvMemorySemanticsUniformMemoryMask:
       return false;
     default:
       break;
@@ -103,8 +150,8 @@ void TransformationLoad::Apply(opt::IRContext* ir_context,
         ir_context, SpvOpAtomicLoad, result_type, message_.fresh_id(),
         opt::Instruction::OperandList(
             {{SPV_OPERAND_TYPE_ID,
-              {message_.pointer_id(), message_.memory_scope(),
-               message_.memory_semantics()}}}));
+              {message_.pointer_id(), message_.memory_scope_id(),
+               message_.memory_semantics_id()}}}));
     auto new_instruction_ptr = new_instruction.get();
     insert_before->InsertBefore(std::move(new_instruction));
     // Inform the def-use manager about the new instruction and record its basic
--- source/fuzz/transformation_load.h
+++ source/fuzz/transformation_load.h
@@ -35,10 +35,10 @@ class TransformationLoad : public Transformation {
   // - |message_.fresh_id| must be fresh
   // - |message_.pointer_id| must be the id of a pointer
   // - |message_.is_atomic| must be true if want to work with OpAtomicLoad
-  // - |message_.memory_scope| must be value to specific scope if the opcode is
-  // OpAtomicLoad, and 0 if opcode is OpLoad
-  // - |message_.memory_semantics| mask we set when work with OpAtomicLoad, and
-  // 0 if opcode is OpLoad
+  // - |message_.memory_scope_id| must be SpvScopeInvocation if is_atomic is true,
+  // 0 otherwise.
+  // - |message_.memory_semantics_id| must be SpvMemorySemanticsWorkgroupMemoryMask
+  // or SpvMemorySemanticsUniformMemoryMask if is_atomic is true, 0 otherwise.
   // - The pointer must not be OpConstantNull or OpUndef
   // - |message_.instruction_to_insert_before| must identify an instruction
   //   before which it is valid to insert an OpLoad, and where
