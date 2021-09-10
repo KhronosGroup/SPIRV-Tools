3b15c5867095e0c10e4292b1293fb675ce138c1c
Steven Perron <stevenperron@google.com>
2021-09-10 10:28:21 -0400

    Handle out-of-bounds accesses in VDCE
    It is possible that other optimization will propagate
    a value into an OpCompositeExtract or OpVectorShuffle
    instruction that is larger than the vector size.
    Vector DCE has to be able to handle it.
    
    Fixes https://github.com/KhronosGroup/SPIRV-Tools/issues/4513.

--- source/opt/vector_dce.h
+++ source/opt/vector_dce.h
@@ -94,11 +94,15 @@ class VectorDCE : public MemPass {
   // Returns true if the result of |inst| is a vector or a scalar.
   bool HasVectorOrScalarResult(const Instruction* inst) const;
 
-  // Returns true if the result of |inst| is a scalar.
+  // Returns true if the result of |inst| is a vector.
   bool HasVectorResult(const Instruction* inst) const;
 
-  // Returns true if the result of |inst| is a vector.
+  // Returns true if the result of |inst| is a scalar.
   bool HasScalarResult(const Instruction* inst) const;
+
+  // Returns the number of elements in the vector type if |type_id| is the id of
+  // a veoctor type. Returns 0 otherwise.
+  uint32_t GetVectorTypeSize(uint32_t type_id);
 
   // Adds |work_item| to |work_list| if it is not already live according to
   // |live_components|.  |live_components| is updated to indicate that
