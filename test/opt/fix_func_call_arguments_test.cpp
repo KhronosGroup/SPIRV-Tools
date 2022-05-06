3e0daa5e6526cf096460684274b4805970b74120
Lu Jiao <lu.jiao@amd.com>
2022-05-06 10:09:07 +0800

    Fix copyright test

--- test/opt/fix_func_call_arguments_test.cpp
+++ test/opt/fix_func_call_arguments_test.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 AMD LLC.
+// Copyright (c) 2022 Advanced Micro Devices, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
