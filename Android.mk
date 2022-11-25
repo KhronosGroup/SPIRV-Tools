00018e58af055a74fd88718af8cca8de34c25106
Greg Fischer <greg@lunarg.com>
2022-11-25 16:48:13 -0700

    Change EliminateDeadInputComponentsPass to EliminateDeadIOComponentsPass (#4997)
    To reflect processing of both Input and Output variables.
    
    Also renamed files as needed.

--- Android.mk
+++ Android.mk
@@ -111,7 +111,7 @@ SPVTOOLS_OPT_SRC_FILES := \
 		source/opt/eliminate_dead_constant_pass.cpp \
 		source/opt/eliminate_dead_functions_pass.cpp \
 		source/opt/eliminate_dead_functions_util.cpp \
-		source/opt/eliminate_dead_input_components_pass.cpp \
+		source/opt/eliminate_dead_io_components_pass.cpp \
 		source/opt/eliminate_dead_members_pass.cpp \
 		source/opt/eliminate_dead_output_stores_pass.cpp \
 		source/opt/feature_manager.cpp \
