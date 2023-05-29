78ff89654a134aa2e26899d6ad58bd3ac82d5571
Alan Baker <alanbaker@google.com>
2023-05-29 15:56:27 -0400

    Error for invalid location type
    Fixes https://crbug.com/oss-fuzz/56754
    
    * When checking locations, produce an error if the type cannot be
      assigned a location

--- source/val/validate_interfaces.cpp
+++ source/val/validate_interfaces.cpp
@@ -174,7 +174,8 @@ spv_result_t NumConsumedLocations(ValidationState_t& _, const Instruction* type,
       break;
     }
     default:
-      break;
+      return _.diag(SPV_ERROR_INVALID_DATA, type)
+             << "Invalid type to assign a location";
   }
 
   return SPV_SUCCESS;
@@ -363,12 +364,12 @@ spv_result_t GetLocationsForVariable(
       sub_type = _.FindDef(sub_type_id);
     }
 
-    for (uint32_t array_idx = 0; array_idx < array_size; ++array_idx) {
-      uint32_t num_locations = 0;
-      if (auto error = NumConsumedLocations(_, sub_type, &num_locations))
-        return error;
+    uint32_t num_locations = 0;
+    if (auto error = NumConsumedLocations(_, sub_type, &num_locations))
+      return error;
+    uint32_t num_components = NumConsumedComponents(_, sub_type);
 
-      uint32_t num_components = NumConsumedComponents(_, sub_type);
+    for (uint32_t array_idx = 0; array_idx < array_size; ++array_idx) {
       uint32_t array_location = location + (num_locations * array_idx);
       uint32_t start = array_location * 4;
       if (kMaxLocations <= start) {
