4eb9670358dabe9a14a36d13dec53f2c4f94b11f
Lu, John <john.lu@intel.com>
2024-05-21 14:03:38 -0700

    Reorder code for clarity
    Signed-off-by: Lu, John <john.lu@intel.com>

--- source/val/validate_extensions.cpp
+++ source/val/validate_extensions.cpp
@@ -2962,10 +2962,6 @@ spv_result_t ValidateExtInst(ValidationState_t& _, const Instruction* inst) {
                  << "expected operand Format to be a pointer";
         }
 
-        // If pointer points to an array, get the type of an element
-        if (_.IsIntArrayType(format_data_type))
-          format_data_type = _.GetComponentType(format_data_type);
-
         if (format_storage_class != spv::StorageClass::UniformConstant &&
             // Extension SPV_EXT_relaxed_printf_string_address_space allows
             // format strings in Global, Local, Private and Generic address
@@ -2984,6 +2980,10 @@ spv_result_t ValidateExtInst(ValidationState_t& _, const Instruction* inst) {
                  << "expected Format storage class to be UniformConstant, "
                     "Crossworkgroup, Workgroup, Function, or Generic";
         }
+
+        // If pointer points to an array, get the type of an element
+        if (_.IsIntArrayType(format_data_type))
+          format_data_type = _.GetComponentType(format_data_type);
 
         if (!_.IsIntScalarType(format_data_type) ||
             _.GetBitWidth(format_data_type) != 8) {
