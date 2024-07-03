3bc9744d0ab9d504971667ae6f4c2889260fcd2e
Victor Lomuller <victor@codeplay.com>
2024-07-03 18:18:40 +0100

    Add FPEncoding operand type. (#5726)
    This patch adds the optional FPEncoding operand that can be added to OpTypeFloat.
    At the moment there is no usable operand, so support is limited to adding the entry.
    
    Co-authored-by: Kévin Petit <kevin.petit@arm.com>
    Co-authored-by: David Neto <dneto@google.com>

--- include/spirv-tools/libspirv.h
+++ include/spirv-tools/libspirv.h
@@ -175,6 +175,7 @@ typedef enum spv_operand_type_t {
   SPV_OPERAND_TYPE_KERNEL_ENQ_FLAGS,              // SPIR-V Sec 3.29
   SPV_OPERAND_TYPE_KERNEL_PROFILING_INFO,         // SPIR-V Sec 3.30
   SPV_OPERAND_TYPE_CAPABILITY,                    // SPIR-V Sec 3.31
+  SPV_OPERAND_TYPE_FPENCODING,                    // SPIR-V Sec 3.51
 
   // NOTE: New concrete enum values should be added at the end.
 
@@ -236,6 +237,8 @@ typedef enum spv_operand_type_t {
   // assemble regardless of where they occur -- literals, IDs, immediate
   // integers, etc.
   SPV_OPERAND_TYPE_OPTIONAL_CIV,
+  // An optional floating point encoding enum
+  SPV_OPERAND_TYPE_OPTIONAL_FPENCODING,
 
   // A variable operand represents zero or more logical operands.
   // In an instruction definition, this may only appear at the end of the
