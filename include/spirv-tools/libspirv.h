8d6ec5d67f655acec6e71dc9503aa6b1a7fa7761
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-09-11 10:50:17 -0500

    Add a Vulkan 1.4 environment

--- include/spirv-tools/libspirv.h
+++ include/spirv-tools/libspirv.h
@@ -577,6 +577,7 @@ typedef enum {
 
   SPV_ENV_UNIVERSAL_1_6,  // SPIR-V 1.6 latest revision, no other restrictions.
   SPV_ENV_VULKAN_1_3,     // Vulkan 1.3 latest revision.
+  SPV_ENV_VULKAN_1_4,     // Vulkan 1.3 latest revision.
 
   SPV_ENV_MAX  // Keep this as the last enum value.
 } spv_target_env;
