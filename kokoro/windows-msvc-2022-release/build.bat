e15ac6102fa103f05de56b9c1cb3080747cd184d
David Neto <dneto@google.com>
2025-01-10 11:21:58 -0500

    kokoro: Add VS 2022 scripts, remove VS 2017 scripts (#5930)

--- kokoro/windows-msvc-2022-release/build.bat
+++ kokoro/windows-msvc-2022-release/build.bat
@@ -1,4 +1,4 @@
-:: Copyright (c) 2018 Google LLC.
+:: Copyright (c) 2025 Google LLC
 ::
 :: Licensed under the Apache License, Version 2.0 (the "License");
 :: you may not use this file except in compliance with the License.
@@ -20,5 +20,5 @@
 set SCRIPT_DIR=%~dp0
 
 :: Call with correct parameter
-call %SCRIPT_DIR%\..\scripts\windows\build.bat RelWithDebInfo 2017
+call %SCRIPT_DIR%\..\scripts\windows\build.bat RelWithDebInfo 2022
 
