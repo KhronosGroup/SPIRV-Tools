812ebe77122146ca433c60a6dc3b2a39a015fd4a
David Neto <dneto@google.com>
2025-04-09 13:06:16 -0400

    format check: Show the list of files to be checked

--- utils/check_code_format.sh
+++ utils/check_code_format.sh
@@ -46,6 +46,9 @@ if [ -z "${FILES_TO_CHECK}" ]; then
   echo "No source code to check for formatting."
   exit 0
 fi
+echo "Checking Formatting for the following files"
+echo "${FILES_TO_CHECK}"
+echo
 
 FORMAT_DIFF=$(git diff -U0 ${BASE_BRANCH} -- ${FILES_TO_CHECK} | python3 "${CLANG_FORMAT_DIFF}" -p1 -style=file -binary "$CLANG_FORMAT")
 
