4d719519bb87baa54f9ec77294b8a68aba10ea68
Boris Zanin <boris.zanin@amd.com>
2025-04-11 10:26:18 +0200

    Update to spec @ 4f3c9f75

--- test/text_to_binary.misc_test.cpp
+++ test/text_to_binary.misc_test.cpp
@@ -61,8 +61,36 @@ TEST_F(TextToBinaryMisc, OpAbortKHR) {
   const auto assembly = R"(
 OpExtension "SPV_KHR_abort"
 OpCapability AbortKHR
-%msg = OpString "abort message"
-OpAbortKHR %msg
+             OpCapability ConstantDataKHR
+
+             OpDecorate %string1_t UTFCodePointsKHR
+             OpDecorate %string2_t UTFCodePointsKHR
+
+             OpDecorate %string1_x UTFCodePointsKHR
+             OpDecorate %string1_x ArrayStride 1
+             OpDecorate %string2_x UTFCodePointsKHR
+             OpDecorate %string2_x ArrayStride 1
+             OpMemberDecorate %message_x 0 Offset 0
+             OpMemberDecorate %message_x 1 Offset 6
+             OpMemberDecorate %message_x 2 Offset 8
+
+   %char_t = OpTypeInt 8 0
+ %uint32_t = OpTypeInt 32 0
+  %str1len = OpConstant %uint32_t 6
+%string1_t = OpTypeArray %char_t %str1len
+  %string1 = OpConstantDataKHR %string_t "test: "
+  %str2len = OpSpecConstant %uint32_t 2
+%string2_t = OpTypeArray %char_t %str2len
+  %string2 = OpSpecConstantDataKHR %string_t "%u"
+%message_t = OpTypeStruct %string1_t %string2_t %uint32_t
+
+%string1_x = OpTypeArray %char_t %str1len
+%string2_x = OpTypeArray %char_t %str2len
+%message_x = OpTypeStruct %string1_t %string2_t %uint32_t
+
+    %abort = OpLabel
+  %message = OpCompositeConstruct %message_t %string1 %string2 %uintval
+             OpAbortKHR %message_x %message
 )";
 
   spv_binary binary = nullptr;
@@ -87,6 +115,57 @@ TEST_F(TextToBinaryMisc, OpAbortKHRInvalidIdName) {
 OpAbortKHR "aaa"
 )";
   EXPECT_THAT(CompileFailure(assembly), Eq("Expected id to start with %."));
+}
+
+TEST_F(TextToBinaryMisc, OpConstantDataKHR) {
+  spv_context context = spvContextCreate(SPV_ENV_UNIVERSAL_1_0);
+  const auto assembly = R"(
+             OpCapability ConstantDataKHR
+
+             OpDecorate %string1_t UTFCodePointsKHR
+             OpDecorate %string2_t UTFCodePointsKHR
+
+             OpDecorate %string1_x UTFCodePointsKHR
+             OpDecorate %string1_x ArrayStride 1
+             OpDecorate %string2_x UTFCodePointsKHR
+             OpDecorate %string2_x ArrayStride 1
+             OpMemberDecorate %message_x 0 Offset 0
+             OpMemberDecorate %message_x 1 Offset 6
+             OpMemberDecorate %message_x 2 Offset 8
+
+   %char_t = OpTypeInt 8 0
+ %uint32_t = OpTypeInt 32 0
+  %str1len = OpConstant %uint32_t 6
+%string1_t = OpTypeArray %char_t %str1len
+  %string1 = OpConstantDataKHR %string_t "test: "
+  %str2len = OpSpecConstant %uint32_t 2
+%string2_t = OpTypeArray %char_t %str2len
+  %string2 = OpSpecConstantDataKHR %string_t "%u"
+%message_t = OpTypeStruct %string1_t %string2_t %uint32_t
+
+%string1_x = OpTypeArray %char_t %str1len
+%string2_x = OpTypeArray %char_t %str2len
+%message_x = OpTypeStruct %string1_t %string2_t %uint32_t
+
+    %abort = OpLabel
+  %message = OpCompositeConstruct %message_t %string1 %string2 %uintval
+)";
+
+  spv_binary binary = nullptr;
+  spv_diagnostic diagnostic = nullptr;
+  EXPECT_EQ(SPV_SUCCESS, spvTextToBinary(context, assembly, strlen(assembly),
+                                         &binary, &diagnostic));
+  EXPECT_NE(nullptr, binary);
+  if (binary) {
+    EXPECT_NE(nullptr, binary->code);
+    EXPECT_NE(0u, binary->wordCount);
+  }
+  if (diagnostic) {
+    spvDiagnosticPrint(diagnostic);
+    ASSERT_TRUE(false);
+  }
+
+  spvContextDestroy(context);
 }
 
 }  // namespace
