0a4be9a7534f0c35c0f155989f85c35663a8d28b
GinShio <ginshio78@gmail.com>
2025-05-07 15:08:02 +0800

    Update testcase

--- test/opt/upgrade_memory_model_test.cpp
+++ test/opt/upgrade_memory_model_test.cpp
@@ -437,14 +437,11 @@ OpFunctionEnd
 TEST_F(UpgradeMemoryModelTest, VariablePointerLoad) {
   const std::string text = R"(
 ; CHECK: OpCapability VulkanMemoryModel
-OpCapability VariablePointersStorageBuffer
 OpCapability Shader
+OpCapability Linkage
+OpCapability VariablePointersStorageBuffer
 OpExtension "SPV_KHR_variable_pointers"
 OpMemoryModel Logical GLSL450
-OpEntryPoint GLCompute %func "main" %var
-OpExecutionMode %func LocalSize 1 1 1
-OpDecorate %var DescriptorSet 1111
-OpDecorate %var Binding 2
 OpDecorate %_runtimearr_uint ArrayStride 4
 OpMemberDecorate %_struct_4 0 Offset 0
 OpDecorate %_struct_4 Block
@@ -453,19 +450,19 @@ OpDecorate %_struct_4 Block
 %_runtimearr_uint = OpTypeRuntimeArray %uint
 %_struct_4 = OpTypeStruct %_runtimearr_uint
 %_ptr_StorageBuffer__struct_4 = OpTypePointer StorageBuffer %_struct_4
-%void = OpTypeVoid
-%10 = OpTypeFunction %void
+%_ptr_Function_uint = OpTypePointer Function %uint
 %_ptr_Function__ptr_StorageBuffer__struct_4 = OpTypePointer Function %_ptr_StorageBuffer__struct_4
+%functy = OpTypeFunction %uint %_ptr_Function__ptr_StorageBuffer__struct_4 %_ptr_Function_uint
 %_ptr_StorageBuffer_uint = OpTypePointer StorageBuffer %uint
-%var = OpVariable %_ptr_StorageBuffer__struct_4 StorageBuffer
-%func = OpFunction %void None %10
-%13 = OpLabel
-%14 = OpVariable %_ptr_Function__ptr_StorageBuffer__struct_4 Function
-OpStore %14 %var
-%15 = OpLoad %_ptr_StorageBuffer__struct_4 %14
-%16 = OpAccessChain %_ptr_StorageBuffer_uint %15 %uint_0 %uint_0
-%17 = OpLoad %uint %16
-OpReturn
+%func = OpFunction %uint None %functy
+%param_buf = OpFunctionParameter %_ptr_Function__ptr_StorageBuffer__struct_4
+%param_offset = OpFunctionParameter %_ptr_Function_uint
+%bb_entry = OpLabel
+%buf = OpLoad %_ptr_StorageBuffer__struct_4 %param_buf
+%offset = OpLoad %uint %param_offset
+%31 = OpAccessChain %_ptr_StorageBuffer_uint %buf %uint_0 %offset
+%data = OpLoad %uint %31
+OpReturnValue %data
 OpFunctionEnd
 )";
   SinglePassRunAndMatch<opt::UpgradeMemoryModel>(text, true);
