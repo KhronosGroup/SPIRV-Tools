427204e2469f2688033d1450bd457f8d934da75a
Alan Baker <alanbaker@google.com>
2025-07-13 12:28:50 -0400

    Fixup variable pointer allocation check and tests'

--- test/fuzz/fuzzer_pass_donate_modules_test.cpp
+++ test/fuzz/fuzzer_pass_donate_modules_test.cpp
@@ -569,6 +569,7 @@ TEST(FuzzerPassDonateModulesTest, DonateOpConstantNull) {
   const auto env = SPV_ENV_UNIVERSAL_1_3;
   const auto consumer = nullptr;
   spvtools::ValidatorOptions validator_options;
+  validator_options.SetRelaxLogicalPointer(true);
 
   const auto recipient_context =
       BuildModule(env, consumer, recipient_shader, kFuzzAssembleOption);
