c62299ab1792fb8b04da6b3a1189fc3813f445b3
Spencer Fricke <115671160+spencer-lunarg@users.noreply.github.com>
2026-01-15 17:21:54 -0500

    spirv-val: Fix crash with bad OpUntypedArrayLengthKHR (#6493)
    working on the internal extension (and being new to Untyped Pointers
    still) I accidentally set the `OpUntypedArrayLengthKHR` Pointer ID to be
    `OpTypeUntypedPointerKHR` and it just crashed in `spirv-val`... so now
    it doesn't crash and provides hopefully a likely good answer for the
    next person like me
    
    ... the issue is `pointer` and `type` are really easy to mix up...
    `OpTypeUntyped` will always be my favorite thing to explain to those new
    to SPIR-V :smile:

--- source/val/validate_memory.cpp
+++ source/val/validate_memory.cpp
@@ -2140,9 +2140,10 @@ spv_result_t ValidateArrayLength(ValidationState_t& state,
   auto pointer_ty_id = state.GetOperandTypeId(inst, (untyped ? 3 : 2));
   auto pointer_ty = state.FindDef(pointer_ty_id);
   if (untyped) {
-    if (pointer_ty->opcode() != spv::Op::OpTypeUntypedPointerKHR) {
+    if (!pointer_ty ||
+        pointer_ty->opcode() != spv::Op::OpTypeUntypedPointerKHR) {
       return state.diag(SPV_ERROR_INVALID_ID, inst)
-             << "Pointer must be an untyped pointer";
+             << "Pointer must be an untyped pointer object";
     }
   } else if (pointer_ty->opcode() != spv::Op::OpTypePointer) {
     return state.diag(SPV_ERROR_INVALID_ID, inst)
