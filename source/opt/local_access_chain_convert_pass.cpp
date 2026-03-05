35ab49433f22f389d3938b6bb353f27e7db03be0
Marijn Suijten <marijns95@gmail.com>
2026-03-05 21:20:49 +0100

    Copy missing SPV_KHR_shader_clock to the other 3 optimization passes

--- source/opt/local_access_chain_convert_pass.cpp
+++ source/opt/local_access_chain_convert_pass.cpp
@@ -448,6 +448,7 @@ void LocalAccessChainConvertPass::InitExtensions() {
       "SPV_KHR_ray_query",
       "SPV_EXT_fragment_invocation_density",
       "SPV_KHR_terminate_invocation",
+      "SPV_KHR_shader_clock",
       "SPV_KHR_subgroup_uniform_control_flow",
       "SPV_KHR_integer_dot_product",
       "SPV_EXT_shader_image_int64",
--- source/opt/local_single_store_elim_pass.cpp
+++ source/opt/local_single_store_elim_pass.cpp
@@ -135,6 +135,7 @@ void LocalSingleStoreElimPass::InitExtensionAllowList() {
       "SPV_EXT_physical_storage_buffer",
       "SPV_KHR_physical_storage_buffer",
       "SPV_KHR_terminate_invocation",
+      "SPV_KHR_shader_clock",
       "SPV_KHR_subgroup_uniform_control_flow",
       "SPV_KHR_integer_dot_product",
       "SPV_EXT_shader_image_int64",
